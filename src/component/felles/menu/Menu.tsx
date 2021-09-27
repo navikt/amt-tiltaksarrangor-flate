c41a8b84cba0a61c618460829a3212b1caf218f6
Alexander Gård <alex.gaard@hotmail.com>
2021-09-27 16:12:01 +0200

    Flytt virksomhet-velger ut i egen komponent

--- src/component/felles/menu/Menu.tsx
+++ src/component/felles/menu/Menu.tsx
@@ -1,49 +1,14 @@
 import styles from './Menu.module.less';
-import { Element, Normaltekst, Systemtittel } from 'nav-frontend-typografi';
-import { useValgtVirksomhetStore } from '../../../store/valgt-virksomhet-store';
-import React, { useMemo } from 'react';
+import { Normaltekst, Systemtittel } from 'nav-frontend-typografi';
+import React from 'react';
 import cls from 'classnames';
 import { useDataStore } from '../../../store/data-store';
-import { useHistory } from 'react-router-dom';
 import { AlertStripeInfo } from 'nav-frontend-alertstriper';
 import navLogo from './nav-logo.svg';
-import Select, { MultiValue, OptionProps, SingleValue } from 'react-select';
-import './VirksomhetVelgerSelect.less';
-
-interface Valg {
-	value: string;
-	label: string;
-}
+import { VirksomhetVelger } from './virksomhet-velger/VirksomhetVelger';
 
 export const Menu = () => {
-	const history = useHistory();
 	const { innloggetAnsatt } = useDataStore();
-	const { valgtVirksomhet, setValgtVirksomhet } = useValgtVirksomhetStore();
-
-	const tilgjengeligeVirksomheter = innloggetAnsatt.virksomheter;
-
-	const onValgtVirksomhetChanged = (valg: SingleValue<Valg> | MultiValue<Valg>) => {
-		const singleValg = valg as SingleValue<Valg>
-
-		const nyValgtVirksomhet = tilgjengeligeVirksomheter.find(virksomhet => virksomhet.id === singleValg?.value);
-
-		if (nyValgtVirksomhet) {
-			setValgtVirksomhet(nyValgtVirksomhet);
-
-			// Når vi bytter virksomhet så redirect til tiltaksinstans-oversikt hvis vi ikke allerede er der
-			if (history.location.pathname !== '/') {
-				history.push('/');
-			}
-		}
-	};
-
-	const virksomhetValg = useMemo(() => {
-		return tilgjengeligeVirksomheter.map(v => ({ value: v.id, label: v.virksomhetsnavn }))
-	}, [tilgjengeligeVirksomheter])
-
-	const selectedValue = useMemo(() => {
-		return virksomhetValg.find(o => o.value === valgtVirksomhet.id)
-	}, [virksomhetValg, valgtVirksomhet])
 
 	return (
 		<header className={styles.menu}>
@@ -62,16 +27,7 @@ export const Menu = () => {
 			<div>
 				<Normaltekst className="blokk-xxs">{`${innloggetAnsatt.fornavn} ${innloggetAnsatt.etternavn}`}</Normaltekst>
 
-				<Select
-					components={{Option: CustomOption}}
-					defaultValue={selectedValue}
-					onChange={onValgtVirksomhetChanged}
-					options={virksomhetValg}
-					placeholder="Velg virksomhet"
-					className="blokk-xxs"
-					classNamePrefix="virksomhet-velger"
-					isSearchable={false}
-				/>
+				<VirksomhetVelger className="blokk-xxs" />
 
 				<a
 					href="/auth-proxy/oauth2/logout?redirect_uri=https://nav.no"
@@ -83,24 +39,3 @@ export const Menu = () => {
 		</header>
 	);
 };
-
-function CustomOption(props: OptionProps<Valg>) {
-	const { innloggetAnsatt } = useDataStore();
-
-	const virksomhet = innloggetAnsatt.virksomheter.find(v => v.id === props.data.value)!;
-
-	return (
-		<div
-			className={cls(styles.optionItem, props.isFocused && styles.optionItemFocused)}
-			onClick={() => props.setValue(props.data, 'select-option', props.data)}
-			role="option"
-			aria-selected={props.isSelected}
-		>
-			<Element className={styles.fontSize14}>{virksomhet.organsisasjonsnavn}</Element>
-			<Normaltekst className={cls(styles.fontSize14, 'blokk-xs')}>Organisasjonsnr: {virksomhet.organsisasjonsnummer}</Normaltekst>
-
-			<Element>{virksomhet.virksomhetsnavn}</Element>
-			<Normaltekst className={styles.fontSize14}>Virksomhetsnr: {virksomhet.virksomhetsnummer}</Normaltekst>
-		</div>
-	)
-}
---
+++ src/component/felles/menu/virksomhet-velger/VirksomhetVelger.tsx
@@ -0,0 +1,87 @@
+import Select, { MultiValue, OptionProps, SingleValue } from 'react-select';
+import React, { useMemo } from 'react';
+import { useHistory } from 'react-router-dom';
+import { useDataStore } from '../../../../store/data-store';
+import { useValgtVirksomhetStore } from '../../../../store/valgt-virksomhet-store';
+import cls from 'classnames';
+import { Element, Normaltekst } from 'nav-frontend-typografi';
+import styles from './VirksomhetVelger.module.less';
+import './VirksomhetVelger.less';
+
+interface Valg {
+	value: string;
+	label: string;
+}
+
+interface VirksomhetVelgerProps {
+	className?: string;
+}
+
+export const VirksomhetVelger = (props: VirksomhetVelgerProps) => {
+	const history = useHistory();
+	const { innloggetAnsatt } = useDataStore();
+	const { valgtVirksomhet, setValgtVirksomhet } = useValgtVirksomhetStore();
+
+	const tilgjengeligeVirksomheter = innloggetAnsatt.virksomheter;
+
+	const onValgtVirksomhetChanged = (valg: SingleValue<Valg> | MultiValue<Valg>) => {
+		const singleValg = valg as SingleValue<Valg>
+
+		const nyValgtVirksomhet = tilgjengeligeVirksomheter.find(virksomhet => virksomhet.id === singleValg?.value);
+
+		if (nyValgtVirksomhet) {
+			setValgtVirksomhet(nyValgtVirksomhet);
+
+			// Når vi bytter virksomhet så redirect til tiltaksinstans-oversikt hvis vi ikke allerede er der
+			if (history.location.pathname !== '/') {
+				history.push('/');
+			}
+		}
+	};
+
+	const virksomhetValg = useMemo(() => {
+		return tilgjengeligeVirksomheter.map(v => ({ value: v.id, label: v.virksomhetsnavn }))
+	}, [tilgjengeligeVirksomheter])
+
+	const selectedValue = useMemo(() => {
+		return virksomhetValg.find(o => o.value === valgtVirksomhet.id)
+	}, [virksomhetValg, valgtVirksomhet])
+
+	return (
+		<Select
+			components={{Option: VirksomhetValgOption}}
+			defaultValue={selectedValue}
+			onChange={onValgtVirksomhetChanged}
+			options={virksomhetValg}
+			placeholder="Velg virksomhet"
+			className={props.className}
+			classNamePrefix="virksomhet-velger"
+			isSearchable={false}
+		/>
+	);
+};
+
+function VirksomhetValgOption(props: OptionProps<Valg>) {
+	const { innloggetAnsatt } = useDataStore();
+
+	const virksomhet = innloggetAnsatt.virksomheter.find(v => v.id === props.data.value);
+
+	if (!virksomhet) {
+		return null;
+	}
+
+	return (
+		<div
+			className={cls(styles.optionItem, props.isFocused && styles.optionItemFocused)}
+			onClick={() => props.setValue(props.data, 'select-option', props.data)}
+			role="option"
+			aria-selected={props.isSelected}
+		>
+			<Element className={styles.fontSize14}>{virksomhet.organsisasjonsnavn}</Element>
+			<Normaltekst className={cls(styles.fontSize14, 'blokk-xs')}>Organisasjonsnr: {virksomhet.organsisasjonsnummer}</Normaltekst>
+
+			<Element>{virksomhet.virksomhetsnavn}</Element>
+			<Normaltekst className={styles.fontSize14}>Virksomhetsnr: {virksomhet.virksomhetsnummer}</Normaltekst>
+		</div>
+	)
+}
