20a58566252964f7a183818483afc1e1141a2c92
Alexander Gård <alex.gaard@hotmail.com>
2021-09-28 15:26:41 +0200

    Flyttet eksplisitt styling import ut til komponent

--- src/component/felles/SpaLenkepanel.tsx
+++ src/component/felles/SpaLenkepanel.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import cls from 'classnames';
 import { Link } from 'react-router-dom';
+import 'nav-frontend-lenkepanel-style'; // Importer direkte siden vi ikke bruker React-komponenten
 
 interface SpaLenkepanelProps {
 	to: string;
