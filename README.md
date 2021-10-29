c6c67798e93a98b3fe4cfedb4816f98d37f28fa9
Alexander Gård <alex.gaard@hotmail.com>
2021-10-29 16:06:57 +0200

    La til mulighet for å kjøre lokalt mot preprod miljø med dev-proxy

--- README.md
+++ README.md
@@ -8,3 +8,20 @@ En demo versjon av applikasjonen kan testes gjennom lenken nedenfor.
 All dataen i demo appen er mocket. Demoen vil i mest mulig grad være lik den reelle applikasjonen, men det kan oppstå differanser.
 
 https://navikt.github.io/amt-tiltaksarrangor-flate/
+
+## Kjør lokalt mot preprod med proxy
+Hvis man ønsker å teste med en reel backend i preprod lokalt så kan man ta i bruk dev-proxyen.
+
+Opprett filen **.env.local** med følgende innhold:
+```.env
+REACT_APP_MOCK_DEV_PROXY_ENABLED=true
+REACT_APP_MOCK_DEV_PROXY_COOKIE=amt-tiltaksarrangor-flate-auth-proxy_session=<SESSION_COOKIE>
+```
+
+Sørg for å hente en ekte session cookie og bytt den ut med `<SESSION_COOKIE>`.
+**.env.local** er lagt til i .gitignore og vil ikke bli commitet.
+
+Kjør opp dev-proxyen med `docker compose up -d` og start deretter applikasjonen med `npm start`.
+
+For å skru av proxyen sett `REACT_APP_MOCK_DEV_PROXY_ENABLED=false` i **.env.local**.
+
---
+++ src/mock/handlers/dev-proxy-handlers.ts
@@ -0,0 +1,42 @@
+import { ResponseComposition, rest, RestContext, RestRequest } from 'msw'
+import { RequestHandler } from 'msw/lib/types/handlers/RequestHandler'
+import { MockedResponse } from 'msw/lib/types/response'
+
+import { getDevProxyCookie, getDevProxyUrl } from '../utils/dev-proxy-env'
+
+// Set mock cookie for all outgoing requests
+document.cookie = getDevProxyCookie()
+
+export const devProxyHandlers: RequestHandler[] = [
+	rest.get('/amt-tiltak/*', async(req, res, ctx) => {
+		return proxyReq(getDevProxyUrl(), req, res, ctx)
+	})
+]
+
+const proxyReq = async(proxyUrl: string, req: RestRequest, res: ResponseComposition, ctx: RestContext): Promise<MockedResponse> => {
+	const proxiedUrl = `${joinUrlAndPath(proxyUrl, req.url.pathname)}${req.url.search}`
+
+	try {
+		const response = await ctx.fetch(proxiedUrl, {
+			credentials: 'include',
+			method: req.method,
+			body: req.body?.toString(),
+			headers: req.headers
+		})
+
+		const bodyText = await response.text()
+
+		return res(ctx.status(response.status), ctx.body(bodyText))
+	} catch (e) {
+		// eslint-disable-next-line no-console
+		console.error('Request to proxy failed', e)
+		return res(ctx.status(500))
+	}
+}
+
+const joinUrlAndPath = (url: string, path: string): string => {
+	url = url.endsWith('/') ? url.substring(0, url.length - 1) : url
+	path = path.startsWith('/') ? path.substring(1) : path
+
+	return `${url}/${path}`
+}
