c9f55151ce349a68daac1836c9762117b6dfe83d
izisfro <izis.froyhov@nav.no>
2021-12-07 14:39:15 +0100

    Forenkler kode som bruker deprecated kommando

--- src/component/page/bruker-detaljer/kopier-knapp/KopierKnapp.tsx
+++ src/component/page/bruker-detaljer/kopier-knapp/KopierKnapp.tsx
@@ -11,24 +11,13 @@ interface KopierKnappProps {
 
 const COPY_TOOLTIP_DURATION_MS = 1000
 
-function writeToClipBoard(text: string): void {
-	const textField = window.document.createElement('textarea')
-
-	textField.innerText = text
-	window.document.body.appendChild(textField)
-
-	textField.select()
-
-	window.document.execCommand('copy')
-	textField.remove()
-}
-
 export const KopierKnapp = (props: KopierKnappProps): React.ReactElement<KopierKnappProps> => {
 	const [ copySuccess, setCopySuccess ] = useState<boolean>(false)
 
 	const handleOnClick = () => {
-		writeToClipBoard(props.text)
-		setCopySuccess(true)
+		navigator.clipboard.writeText(props.text)
+			.then(() => setCopySuccess(true))
+			.catch(()=> setCopySuccess(false))
 	}
 
 	useEffect(() => {
