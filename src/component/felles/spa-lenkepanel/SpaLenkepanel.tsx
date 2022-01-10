91a3d7ded622c4252fe041ba4251954dfabc8036
Alexander Gård <alex.gaard@hotmail.com>
2022-01-10 16:18:59 +0100

    Bruk paneler fra nytt designsystem

---
+++ src/component/felles/spa-lenkepanel/SpaLenkepanel.tsx
@@ -0,0 +1,28 @@
+import { Next } from '@navikt/ds-icons'
+import cls from 'classnames'
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import styles from './SpaLenkepanel.module.less'
+
+interface SpaLenkepanelProps {
+	to: string;
+	children?: React.ReactNode;
+	className?: string;
+}
+
+export const SpaLenkepanel = (props: SpaLenkepanelProps): React.ReactElement<SpaLenkepanelProps> => {
+	const { to, children, className } = props
+
+	return (
+		<Link to={to} className={cls('navds-link-panel', styles.spaLenkepanel, className)}>
+			<div className="navds-link-panel__content">
+				{children}
+			</div>
+			<Next
+				className="navds-link-panel__chevron"
+				aria-label="arrow-icon pointing right"
+			/>
+		</Link>
+	)
+}
