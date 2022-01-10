b7e5f5016b241f276bc6f29d05aacc0769a2e2a2
Alexander Gård <alex.gaard@hotmail.com>
2022-01-10 10:56:00 +0100

    Refaktorerte InfoElement
    Bruk children istedenfor å sette tekst som en string

--- src/component/page/informasjon-page/InformasjonPage.tsx
+++ src/component/page/informasjon-page/InformasjonPage.tsx
@@ -10,9 +10,9 @@ import mobilephoneImg from './mobilephone.svg'
 
 interface InfoElementProps {
 	title: string
-	text: string
 	image: string
 	alt: string
+	children?: React.ReactNode
 }
 
 export const InfoElement = (props: InfoElementProps): React.ReactElement<InfoElementProps> => {
@@ -21,7 +21,7 @@ export const InfoElement = (props: InfoElementProps): React.ReactElement<InfoEle
 			<img className={styles.infoElementImg} src={props.image} alt={props.alt}/>
 			<div>
 				<Heading className={styles.infoElementTitle} spacing size="xsmall" level="3">{props.title}</Heading>
-				<BodyLong className={styles.infoElementText} spacing>{props.text}</BodyLong>
+				{props.children}
 			</div>
 		</div>
 	)
@@ -42,24 +42,37 @@ export const InformasjonPage = (): React.ReactElement => {
 
 					<InfoElement
 						title="Ikke del kontaktinformasjon videre"
-						text="Kontaktinformasjonen til NAV-veileder skal ikke deles videre til deltaker.  Deltakere som ønsker å kontakte sin NAV-veileder skal logge inn på nav.no eller ringe NAV kontaktsenter."
 						image={mobilephoneImg}
 						alt="Mobiltelefon ikon"
-					/>
+					>
+						<BodyLong className={styles.infoElementText} spacing>
+							Kontaktinformasjonen til NAV-veileder skal ikke deles videre til deltaker.
+							Deltakere som ønsker å kontakte sin NAV-veileder skal logge inn på nav.no eller ringe NAV kontaktsenter.
+						</BodyLong>
+					</InfoElement>
 
 					<InfoElement
 						title="Deltakere som er ferdig i tiltaket"
-						text="Deltakere som er merkert med “Har sluttet” eller “Ikke aktuell”, vises i 14 dager før de fjernes. Deltakerne vises etter at de har sluttet slik at ansatte hos tiltaksarrangør skal ha tilstrekkelig med tid til å se at en person skal ut av tiltaket. "
 						image={clipboardImg}
 						alt="Utklippstavle ikon"
-					/>
+					>
+						<BodyLong className={styles.infoElementText} spacing>
+							Deltakere som er merkert med “Har sluttet” eller “Ikke aktuell”, vises i 14 dager før de fjernes.
+							Deltakerne vises etter at de har sluttet slik at ansatte hos tiltaksarrangør skal ha tilstrekkelig med tid til å se at en person skal ut av tiltaket.
+						</BodyLong>
+					</InfoElement>
 
 					<InfoElement
 						title="Løsningen er under arbeid"
-						text="Det jobbes kontinuerlig med å gjøre tjenesten bedre. Vi som lager tjenesten ønsker gjerne tilbakemeldinger på utfordringer dere opplever i kommunikasjonen mellom NAV, deltaker og tiltaksarrangør. Gi tilbakemelding ved å klikke på “Tilbakemelding” som ligger til høyre. "
 						image={lightbulbImg}
 						alt="Lyspære ikon"
-					/>
+					>
+						<BodyLong className={styles.infoElementText} spacing>
+							Det jobbes kontinuerlig med å gjøre tjenesten bedre.
+							Vi som lager tjenesten ønsker gjerne tilbakemeldinger på utfordringer dere opplever i kommunikasjonen mellom NAV, deltaker og tiltaksarrangør.
+							Gi tilbakemelding ved å klikke på “Tilbakemelding” som ligger til høyre.
+						</BodyLong>
+					</InfoElement>
 				</Card>
 			</div>
 
