17d2390ac29342636b94942ffaec28c2625bc89c
Alexander Gård <alex.gaard@hotmail.com>
2022-01-10 16:18:59 +0100

    Bruk paneler fra nytt designsystem

--- src/component/page/gjennomforing-page/gjennomforing-liste/GjennomforingListePanel.tsx
+++ src/component/page/gjennomforing-page/gjennomforing-liste/GjennomforingListePanel.tsx
@@ -2,7 +2,7 @@ import { Undertittel } from 'nav-frontend-typografi'
 import React from 'react'
 
 import { internalUrl } from '../../../../utils/url-utils'
-import { SpaLenkepanel } from '../../../felles/SpaLenkepanel'
+import { SpaLenkepanel } from '../../../felles/spa-lenkepanel/SpaLenkepanel'
 import styles from './GjennomforingListePanel.module.less'
 
 interface GjennomforingListePanelProps {
@@ -14,7 +14,7 @@ export const GjennomforingListePanel = (props: GjennomforingListePanelProps): Re
 	const { id, navn } = props
 
 	return (
-		<SpaLenkepanel to={internalUrl(`/gjennomforing/${id}`)} border>
+		<SpaLenkepanel to={internalUrl(`/gjennomforing/${id}`)}>
 			<div className={styles.content} >
 				<Undertittel>{navn}</Undertittel>
 			</div>
