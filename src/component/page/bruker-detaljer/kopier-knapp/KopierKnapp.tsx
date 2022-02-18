84476281e2d4b061da468a0c71d007feed3f590c
izisfro <izis.froyhov@nav.no>
2022-02-18 13:31:15 +0100

    Logger klikk

--- src/component/page/bruker-detaljer/kopier-knapp/KopierKnapp.tsx
+++ src/component/page/bruker-detaljer/kopier-knapp/KopierKnapp.tsx
@@ -2,6 +2,7 @@ import { Button } from '@navikt/ds-react'
 import cls from 'classnames'
 import React, { useEffect, useState } from 'react'
 
+import { loggKlikk } from '../../../../utils/amplitude-utils'
 import styles from './KopierKnapp.module.scss'
 
 interface KopierKnappProps {
@@ -19,6 +20,8 @@ export const KopierKnapp = (props: KopierKnappProps): React.ReactElement<KopierK
 		navigator.clipboard.writeText(props.text)
 			.then(() => setCopySuccess(true))
 			.catch(()=> setCopySuccess(false))
+
+		loggKlikk('KopierFnrKnapp')
 	}
 
 	useEffect(() => {
--- src/utils/amplitude-utils.ts
+++ src/utils/amplitude-utils.ts
@@ -38,6 +38,11 @@ const logAmplitudeEvent = (eventName: string, data?: { [key: string]: EventDataV
 	})
 }
 
-export const logSidevisning = (pathname: string): void => {
+export const loggSidevisning = (pathname: string): void => {
 	logAmplitudeEvent('sidevisning', { path: pathname })
+}
+
+export const loggKlikk = (komponent: string, verdi?: string, status?: string) => {
+	const eventKategori = 'Brukerklikk'
+	logAmplitudeEvent('navigasjon', { eventKategori, komponent, verdi, status })
 }
