0aa43949e87ccd54962adc379bcaf82da9a88e27
izisfro <izis.froyhov@nav.no>
2022-02-18 16:15:33 +0100

    Bytter om event navn og type for å få bedre grafer

--- src/component/page/gjennomforing-detaljer/deltaker-oversikt/Rad.tsx
+++ src/component/page/gjennomforing-detaljer/deltaker-oversikt/Rad.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 
 import { TiltakDeltaker } from '../../../../domeneobjekter/deltaker'
 import { brukerDetaljerPageUrl } from '../../../../navigation'
-import { loggKlikk } from '../../../../utils/amplitude-utils'
+import { klikkDeltakerRadOversikt, loggKlikk } from '../../../../utils/amplitude-utils'
 import { lagKommaSeparertBrukerNavn } from '../../../../utils/bruker-utils'
 import { formatDate } from '../../../../utils/date-utils'
 import { StatusMerkelapp } from '../../../felles/status-merkelapp/StatusMerkelapp'
@@ -21,7 +21,7 @@ export const Rad = (props: RadProps): React.ReactElement<RadProps> => {
 	return (
 		<Table.Row key={id}>
 			<Table.DataCell>
-				<Link className={styles.brukersNavn} to={brukerDetaljerPageUrl(id)} onClick={()=> loggKlikk('deltaker-rad')} >
+				<Link className={styles.brukersNavn} to={brukerDetaljerPageUrl(id)} onClick={()=> loggKlikk(klikkDeltakerRadOversikt)} >
 					{lagKommaSeparertBrukerNavn(fornavn, etternavn)}
 				</Link>
 			</Table.DataCell>
