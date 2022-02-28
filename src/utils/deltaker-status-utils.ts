27cbbbd0f99ca78f5f6129184bfa4d71a416718f
Alexander Gård <alex.gaard@hotmail.com>
2022-02-28 14:05:02 +0100

    Deltakere skal skjules etter 2 uker

--- src/utils/deltaker-status-utils.ts
+++ src/utils/deltaker-status-utils.ts
@@ -15,7 +15,7 @@ export const getAntallDeltakerePerStatus = (deltakere: TiltakDeltaker[]): Map<Ti
 }
 
 export const deltakerSkalSkjulesFra = (status: DeltakerStatus) => {
-	return dayjs(status.endretDato).add(4, 'd')
+	return dayjs(status.endretDato).add(2, 'w')
 }
 
 export const sluttaForOver2UkerSiden = (status : DeltakerStatus) => {
