b91dcfac6ced589995128d3d83ae6dfec6e52d77
Alexander Gård <alex.gaard@hotmail.com>
2022-03-16 13:52:11 +0100

    La til fast bredde på kolonner i deltaker tabell

--- src/component/page/gjennomforing-detaljer/deltaker-oversikt/TabellHeader.tsx
+++ src/component/page/gjennomforing-detaljer/deltaker-oversikt/TabellHeader.tsx
@@ -4,16 +4,25 @@ import React from 'react'
 import { SorterbarKolonneHeader } from './SorterbarKolonneHeader'
 import { DeltakerKolonneNavn } from './types'
 
+function dynamicWidth(minPixels: number): React.CSSProperties {
+	return { minWidth: minPixels + 'px' }
+}
+
+function fixedWidth(pixels: number): React.CSSProperties {
+	const width =  pixels + 'px'
+	return { width: width , minWidth: width }
+}
+
 export const TabellHeader = (): JSX.Element => {
 	return (
 		<Table.Header>
 			<Table.Row>
-				<SorterbarKolonneHeader kolonne={DeltakerKolonneNavn.NAVN}/>
-				<SorterbarKolonneHeader kolonne={DeltakerKolonneNavn.FODSELSNUMMER}/>
-				<SorterbarKolonneHeader kolonne={DeltakerKolonneNavn.REGDATO} />
-				<SorterbarKolonneHeader kolonne={DeltakerKolonneNavn.OPPSTART} />
-				<SorterbarKolonneHeader kolonne={DeltakerKolonneNavn.SLUTT} />
-				<SorterbarKolonneHeader kolonne={DeltakerKolonneNavn.STATUS} />
+				<SorterbarKolonneHeader kolonne={DeltakerKolonneNavn.NAVN} style={dynamicWidth(230)}/>
+				<SorterbarKolonneHeader kolonne={DeltakerKolonneNavn.FODSELSNUMMER} style={fixedWidth(128)}/>
+				<SorterbarKolonneHeader kolonne={DeltakerKolonneNavn.REGDATO} style={fixedWidth(120)} />
+				<SorterbarKolonneHeader kolonne={DeltakerKolonneNavn.OPPSTART} style={fixedWidth(120)} />
+				<SorterbarKolonneHeader kolonne={DeltakerKolonneNavn.SLUTT} style={fixedWidth(120)} />
+				<SorterbarKolonneHeader kolonne={DeltakerKolonneNavn.STATUS} style={fixedWidth(168)} />
 			</Table.Row>
 		</Table.Header>
 	)
