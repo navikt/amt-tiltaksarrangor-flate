5a70edb898736759115f5824a730550cdb23c94a
Alexander Gård <AlexGaard@users.noreply.github.com>
2022-03-17 09:19:09 +0100

    Endret på formatering av telefonnummer (#109)

--- src/utils/bruker-utils.test.ts
+++ src/utils/bruker-utils.test.ts
@@ -6,13 +6,17 @@ test('formaterTelefonnummer skal returnere tom string hvis null | undefined | to
 	expect(formaterTelefonnummer('')).toEqual('')
 })
 
-test('formaterTelefonnummer skal parse norskt telefonnummer (8 siffer)', () => {
-	expect(formaterTelefonnummer('12345678')).toEqual('123 45 678')
-	expect(formaterTelefonnummer('123 45 678')).toEqual('123 45 678')
+test('formaterTelefonnummer skal parse norskt telefonnummer', () => {
+	expect(formaterTelefonnummer('+47 12345678')).toEqual('12 34 56 78')
+	expect(formaterTelefonnummer('12345678')).toEqual('12 34 56 78')
 })
 
-test('formaterTelefonnummer skal ikke endre telefonnummer som ikke har 8 siffer', () => {
+test('formaterTelefonnummer skal ikke endre telefonnummer som ikke har skille mellom landskode og nummer', () => {
 	expect(formaterTelefonnummer('+4712345678')).toEqual('+4712345678')
+})
+
+test('formaterTelefonnummer skal ikke endre telefonnummer som ikke er norskt telefonnummer', () => {
+	expect(formaterTelefonnummer('+49 12345678')).toEqual('+49 12345678')
 	expect(formaterTelefonnummer('123456789')).toEqual('123456789')
 	expect(formaterTelefonnummer('123 456 789')).toEqual('123 456 789')
 })
