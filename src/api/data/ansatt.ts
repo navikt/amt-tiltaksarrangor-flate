29af01ad3485013207baa8b17c69bd1c9ad97060
Alexander Gård <alex.gaard@hotmail.com>
2022-03-25 14:07:03 +0100

    Gjør overordnet enhet/orgnr til nullable

--- src/api/data/ansatt.ts
+++ src/api/data/ansatt.ts
@@ -2,8 +2,8 @@ import { z } from 'zod'
 
 export const virksomhetSchema = z.object({
 	id: z.string().uuid(),
-	overordnetEnhetOrganisasjonsnummer: z.string(),
-	overordnetEnhetNavn: z.string(),
+	overordnetEnhetOrganisasjonsnummer: z.string().nullable(),
+	overordnetEnhetNavn: z.string().nullable(),
 	organisasjonsnummer: z.string(),
 	navn: z.string(),
 	roller: z.array(z.string()),
