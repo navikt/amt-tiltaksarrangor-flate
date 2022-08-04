5932e3ee2a6347ed428f05457379abaa7133ca86
izisfro <izis.froyhov@nav.no>
2022-08-04 13:21:59 +0200

    Setter timeout for sesjon alert basert på expiration date

--- src/component/sesjon-notifikasjon/SesjonNotifikasjon.tsx
+++ src/component/sesjon-notifikasjon/SesjonNotifikasjon.tsx
@@ -1,57 +1,66 @@
 import { Alert, Link } from '@navikt/ds-react'
 import { AxiosError, AxiosResponse } from 'axios'
 import dayjs from 'dayjs'
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 import { hentAuthInfo } from '../../api/auth-api'
 import { AuthInfo } from '../../api/data/auth'
 import { loginUrl } from '../../utils/url-utils'
-import { isResolved, usePromise } from '../../utils/use-promise'
+import { usePromise } from '../../utils/use-promise'
 import styles from './SesjonNotifikasjon.module.scss'
 
 enum SesjonStatus {
 	UTLOPER_SNART,
-	TVUNGEN_UTLOGGING
+	TVUNGEN_UTLOGGING_SNART
 }
 
 export const SesjonNotifikasjon = (): React.ReactElement | null => {
 	const fetchAuthInfo = usePromise<AxiosResponse<AuthInfo>, AxiosError>(hentAuthInfo)
+	const expirationTime = fetchAuthInfo.result?.data.expirationTime
+	const tvungenUtloggingTimeoutRef = useRef<number>()
 	const [ sesjonStatus, setSesjonStatus ] = React.useState<SesjonStatus>()
-	const tvungenLogUtloggingTimeout = useRef<number>()
-	const expirationTime = useRef<Date | null>()
-	const visTvungen = sesjonStatus === SesjonStatus.TVUNGEN_UTLOGGING
+	const [ utlopOmMs, setUtlopOmMs ] = useState<number>()
+	const [ utloggingOmMs, setUtloggingOmMs ] = useState<number>()
+	const visTvungen = sesjonStatus === SesjonStatus.TVUNGEN_UTLOGGING_SNART
 	const visUtloper = sesjonStatus === SesjonStatus.UTLOPER_SNART
 
-	const visTvungenAvslutning = () => !!expirationTime.current && dayjs(expirationTime.current)
-		.subtract(30, 'seconds')
-		.isBefore(dayjs())
+	useEffect(() => {
+		if(expirationTime === undefined) return
+		const now = dayjs()
+		const msTilUtlop =  dayjs(expirationTime)
+			.subtract(5, 'minutes').diff(now)
 
-	const visUtloperSnart = () => !!expirationTime.current && dayjs(expirationTime.current)
-		.subtract(5, 'minutes')
-		.isBefore(dayjs())
+		const msTilUtlogging = dayjs(expirationTime)
+			.subtract(40, 'seconds').diff(now)
+
+		setUtlopOmMs(msTilUtlop > 0? msTilUtlop : 0)
+		setUtloggingOmMs(msTilUtlogging > 0? msTilUtlogging : 0)
+
+	}, [ expirationTime ])
 
 	useEffect(() => {
-		setInterval(() => {
-			if (visTvungenAvslutning())
-				setSesjonStatus(SesjonStatus.TVUNGEN_UTLOGGING)
-			else if (visUtloperSnart())
-				setSesjonStatus(SesjonStatus.UTLOPER_SNART)
+		if(utlopOmMs === undefined) return
 
-		}, 5000)
+		setTimeout(() => {
+			setSesjonStatus(SesjonStatus.UTLOPER_SNART)
+		}, utlopOmMs)
 
-	}, [])
+	}, [ utlopOmMs ])
 
 	useEffect(() => {
-		if (isResolved(fetchAuthInfo)){
-			expirationTime.current = fetchAuthInfo.result.data.expirationTime
-		}
-	}, [ fetchAuthInfo ])
+		if(utloggingOmMs === undefined) return
+
+		setTimeout(() => {
+			setSesjonStatus(SesjonStatus.TVUNGEN_UTLOGGING_SNART)
+		}, utloggingOmMs)
+
+	}, [ utloggingOmMs ])
 
 	useEffect(() => {
-		if (tvungenLogUtloggingTimeout.current) return
-		if (sesjonStatus !== SesjonStatus.TVUNGEN_UTLOGGING) return
+		if (sesjonStatus !== SesjonStatus.TVUNGEN_UTLOGGING_SNART) return
+		if (tvungenUtloggingTimeoutRef.current) return
 
-		tvungenLogUtloggingTimeout.current = setTimeout(() => {
+		tvungenUtloggingTimeoutRef.current = setTimeout(() => {
 			window.location.href = loginUrl()
 		}, 10000) as unknown as number
 
@@ -67,6 +76,5 @@ export const SesjonNotifikasjon = (): React.ReactElement | null => {
 			</Alert>
 		}
 	</div>
-
 
 }
