1a4617298e45b7df2759d24941a58f1460c077e3
Tormod Fossum <tormod.fossum@nav.no>
2022-10-18 14:38:34 +0200

    Fiks ingress og cookies for kobling mot dev

--- README.md
+++ README.md
@@ -24,11 +24,17 @@ så sett følgende innhold i **.env.local**:
 
 ```.env
 REACT_APP_MOCK_REQUEST_HANDLER=dev
-REACT_APP_MOCK_REQUEST_COOKIE=io.nais.wonderwall.session=<SESSION_COOKIE>
+REACT_APP_MOCK_REQUEST_COOKIE="<ALL REQUEST COOKIES>"
 ```
 
-Sørg for å hente en ekte session cookie og bytt den ut med `<SESSION_COOKIE>`.
+Logg inn i testmiljø og hent alle cookies fra request header og bytt de ut med `<ALL REQUEST COOKIES>`
 **.env.local** er lagt til i .gitignore og vil ikke bli commitet.
+
+### Eksempel:
+```.env
+REACT_APP_MOCK_REQUEST_HANDLER=dev
+REACT_APP_MOCK_REQUEST_COOKIE="io.nais.wonderwall.session=Fx8oqzvi5FhS5y4TYHXIYliXHT9fNs6/EXAMPLE/dgAxwY5H9S1qrdgg9E4Y+xv76lmv4bWggPZLr3hTZ12mViRYskEeGxpOPf8UvceXxOy5oYOESdLI; innloggingsstatus-token=eyJhbGciOiJIUzUxMiJ9.eyJhdWQiOiJpbm5sb2dnaW5nc3N0YXR1cy1kZXYiLCJzdWIiOiIyNzg0OTA5ODE3NCIsImFjciI6IkxldmVsNCIsImlzcyI6ImlubmxvZ2dpbmdzc3RhdHVzLWRldiIsImV4cCI6MTY2NjA5ODcwMywiaWF0IjoxNjY2MDk1MTAzfQ.p4sXJFdopUa9s2PzzTzNLT-7easXEKMq9Bi7vhe69AUjlDsioSsKW7P3TPs3j5c3MALExample"
+```
 
 Kjør opp dev-proxyen med `docker compose up -d` og start deretter applikasjonen med `npm start`.
 
--- src/mock/handlers/dev-proxy-handlers.ts
+++ src/mock/handlers/dev-proxy-handlers.ts
@@ -3,31 +3,36 @@ import { RequestHandler } from 'msw/lib/types/handlers/RequestHandler'
 import { MockedResponse } from 'msw/lib/types/response'
 
 import { appUrl } from '../../utils/url-utils'
-import { getProxyUrl,getRequestCookie } from '../utils/mock-env'
+import { getProxyUrl,  getRequestCookie } from '../utils/mock-env'
 import { joinUrlAndPath } from '../utils/url-utils'
 
 // Set mock cookie for all outgoing requests
-document.cookie = getRequestCookie()
+getRequestCookie()
+	.split(';')
+	.forEach(c => document.cookie = c.trim() + '; path=/')
 
 export const devProxyHandlers: RequestHandler[] = [
 	rest.get(appUrl('/amt-tiltak/*'), async(req, res, ctx) => {
+        
 		return handleReq(getProxyUrl(), req, res, ctx)
 	})
+   
 ]
 
-const handleReq = async(proxyUrl: string, req: RestRequest, res: ResponseComposition, ctx: RestContext): Promise<MockedResponse> => {
+const handleReq = async(proxyUrl: string, req: RestRequest, res: ResponseComposition, ctx: RestContext): Promise<MockedResponse> => {    
 	const proxiedUrl = `${joinUrlAndPath(proxyUrl, req.url.pathname)}${req.url.search}`
 
 	try {
-		const response = await ctx.fetch(proxiedUrl, {
+       
+		const response = await ctx.fetch(proxiedUrl,  {
 			credentials: 'include',
 			method: req.method,
 			body: req.body?.toString(),
 			headers: req.headers
 		})
-
+            
 		const bodyText = await response.text()
-
+            
 		return res(ctx.status(response.status), ctx.body(bodyText))
 	} catch (e) {
 		// eslint-disable-next-line no-console
