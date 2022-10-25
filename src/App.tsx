ac1418decc2a31f0cd77b777bb786e2781e52863
Magnus Evensberget <magnus.evensberget@nav.no>
2022-10-25 11:18:21 +0200

    Navigasjonsproblemer når bruker ikke har tilgang - Fikset problemer med p i ul - Navigasjonsproblemer når bruker ikke har tilgang

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { fetchInnloggetAnsatt } from './api/tiltak-api'
 import { Banner } from './component/felles/menu/Banner'
 import { SpinnerPage } from './component/felles/spinner-page/SpinnerPage'
 import { LandingPageView } from './component/page/landing-page/LandingPage'
-import { INGEN_ROLLE_PAGE_ROUTE } from './navigation'
+import { INGEN_ROLLE_PAGE_ROUTE, rollePaakrevet } from './navigation'
 import { PrivateRoutes, PublicRoutes } from './Routes'
 import { useAuthStore } from './store/data-store'
 import { isNotStartedOrPending, isRejected, isResolved, usePromise } from './utils/use-promise'
@@ -15,27 +15,32 @@ import { isNotStartedOrPending, isRejected, isResolved, usePromise } from './uti
 export const App = (): React.ReactElement => {
 	const fetchInnloggetAnsattPromise = usePromise<AxiosResponse<InnloggetAnsatt>, AxiosError>(fetchInnloggetAnsatt)
 	const erIkkeInnlogget = fetchInnloggetAnsattPromise.error?.response?.status === 401
-	const { setInnloggetAnsatt } = useAuthStore()
+	const { innloggetAnsatt, setInnloggetAnsatt } = useAuthStore()
 	const navigate = useNavigate()
 	const location = useLocation()
 
 	useEffect(() => {
 		if (isResolved(fetchInnloggetAnsattPromise)) {
-			const innloggetAnsatt = fetchInnloggetAnsattPromise.result.data
-
-			setInnloggetAnsatt(innloggetAnsatt)
-
-			const harTilgang = innloggetAnsatt.arrangorer
-				.filter(arrangor => arrangor.roller.length > 0)
-				.length > 0
-
-			if (!harTilgang && !matchPath(INGEN_ROLLE_PAGE_ROUTE, location.pathname)) {
-				navigate(INGEN_ROLLE_PAGE_ROUTE, { replace: true })
-			}
+			setInnloggetAnsatt(fetchInnloggetAnsattPromise.result.data)
 		}
-
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [ fetchInnloggetAnsattPromise ])
+
+	useEffect(() => {
+		if (!innloggetAnsatt) return
+
+		const harTilgang = innloggetAnsatt.arrangorer
+			.filter(arrangor => arrangor.roller.length > 0)
+			.length > 0
+
+		const trengerRolle = rollePaakrevet(location.pathname)
+		const erPaaIngenRollePage = matchPath(INGEN_ROLLE_PAGE_ROUTE, location.pathname)
+
+		if (trengerRolle && !harTilgang && !erPaaIngenRollePage) {
+			navigate(INGEN_ROLLE_PAGE_ROUTE, { replace: true })
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [ innloggetAnsatt, location.pathname ])
 
 	if (isNotStartedOrPending(fetchInnloggetAnsattPromise)) {
 		return <SpinnerPage/>
