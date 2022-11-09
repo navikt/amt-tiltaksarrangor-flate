03ae92242a67502869547cb12ad80feab29c3f53
Magnus Evensberget <magnus.evensberget@nav.no>
2022-11-09 13:36:33 +0100

    Bytte kall mot amt-tiltak til nye endepunkt

--- src/api/tiltak-api.ts
+++ src/api/tiltak-api.ts
@@ -21,7 +21,7 @@ import { axiosInstance, logAndThrowError, parse } from './utils'
 
 export const fetchInnloggetAnsatt = (): AxiosPromise<InnloggetAnsatt> => {
 	return axiosInstance
-		.get(appUrl('/amt-tiltak/api/arrangor/ansatt/meg'))
+		.get(appUrl('/amt-tiltak/api/tiltaksarrangor/ansatt/meg'))
 		.then(parse(innloggetAnsattSchema))
 		.catch(err => {
 			// Ikke logg 401 feil til sentry
@@ -37,7 +37,7 @@ export const fetchInnloggetAnsatt = (): AxiosPromise<InnloggetAnsatt> => {
 // Henter gjennomføringer som er lagt til i oversikten
 export const fetchTiltakGjennomforinger = (): AxiosPromise<Gjennomforing[]> => {
 	return axiosInstance
-		.get(appUrl('/amt-tiltak/api/gjennomforing'))
+		.get(appUrl('/amt-tiltak/api/tiltaksarrangor/gjennomforing'))
 		.then(parse(gjennomforingerSchema))
 		.catch(logAndThrowError)
 }
@@ -45,47 +45,47 @@ export const fetchTiltakGjennomforinger = (): AxiosPromise<Gjennomforing[]> => {
 // Henter alle gjennomføringer som er tilgjengelig, noen gjennomføringer kan allerede være lagt til i oversikten
 export const fetchTilgjengeligGjennomforinger = (): AxiosPromise<Gjennomforing[]> => {
 	return axiosInstance
-		.get(appUrl('/amt-tiltak/api/gjennomforing/tilgjengelig'))
+		.get(appUrl('/amt-tiltak/api/tiltaksarrangor/gjennomforing/tilgjengelig'))
 		.then(parse(gjennomforingerSchema))
 		.catch(logAndThrowError)
 }
 
 export const opprettTilgangTilGjennomforing = (gjennomforingId: string): AxiosPromise => {
 	return axiosInstance
-		.post(appUrl(`/amt-tiltak/api/gjennomforing/${gjennomforingId}/tilgang`))
+		.post(appUrl(`/amt-tiltak/api/tiltaksarrangor/gjennomforing/${gjennomforingId}/tilgang`))
 		.catch(logAndThrowError)
 }
 
 export const fjernTilgangTilGjennomforing = (gjennomforingId: string): AxiosPromise => {
 	return axiosInstance
-		.delete(appUrl(`/amt-tiltak/api/gjennomforing/${gjennomforingId}/tilgang`))
+		.delete(appUrl(`/amt-tiltak/api/tiltaksarrangor/gjennomforing/${gjennomforingId}/tilgang`))
 		.catch(logAndThrowError)
 }
 
 export const fetchTiltakGjennomforing = (gjennomforingId: string): AxiosPromise<Gjennomforing> => {
 	return axiosInstance
-		.get(appUrl(`/amt-tiltak/api/gjennomforing/${gjennomforingId}`))
+		.get(appUrl(`/amt-tiltak/api/tiltaksarrangor/gjennomforing/${gjennomforingId}`))
 		.then(parse(gjennomforingSchema))
 		.catch(logAndThrowError)
 }
 
 export const fetchKoordinatorerForGjennfomforing = (gjennomforingId: string): AxiosPromise<Koordinator[]> => {
 	return axiosInstance
-		.get(appUrl(`/amt-tiltak/api/gjennomforing/${gjennomforingId}/koordinatorer`))
+		.get(appUrl(`/amt-tiltak/api/tiltaksarrangor/gjennomforing/${gjennomforingId}/koordinatorer`))
 		.then(parse(koordinatorListSchema))
 		.catch(logAndThrowError)
 }
 
 export const fetchDeltakerePaTiltakGjennomforing = (gjennomforingId: string): AxiosPromise<TiltakDeltaker[]> => {
 	return axiosInstance
-		.get(appUrl(`/amt-tiltak/api/gjennomforing/${gjennomforingId}/deltakere`))
+		.get(appUrl(`/amt-tiltak/api/tiltaksarrangor/gjennomforing/${gjennomforingId}/deltakere`))
 		.then(parse(tiltakDeltakereSchema))
 		.catch(logAndThrowError)
 }
 
 export const fetchTiltakDeltagerDetaljer = (tiltakDeltagerId: string): AxiosPromise<TiltakDeltakerDetaljer> => {
 	return axiosInstance
-		.get(appUrl(`/amt-tiltak/api/tiltak-deltaker/${tiltakDeltagerId}`))
+		.get(appUrl(`/amt-tiltak/api/tiltaksarrangor/deltaker/${tiltakDeltagerId}`))
 		.then(parse(tiltakDeltakerDetaljerSchema))
 		.catch(logAndThrowError)
 }
--- src/mock/handlers/mock-handlers.ts
+++ src/mock/handlers/mock-handlers.ts
@@ -15,20 +15,20 @@ import { mockInnloggetAnsatt } from '../data/ansatt'
 import { randomUuid } from '../utils/faker'
 
 export const mockHandlers: RequestHandler[] = [
-	rest.get(appUrl('/amt-tiltak/api/arrangor/ansatt/meg'), (_req, res, ctx) => {
+	rest.get(appUrl('/amt-tiltak/api/tiltaksarrangor/ansatt/meg'), (_req, res, ctx) => {
 		return res(ctx.delay(500), ctx.json(mockInnloggetAnsatt))
 	}),
-	rest.get(appUrl('/amt-tiltak/api/gjennomforing/tilgjengelig'), (_req, res, ctx) => {
+	rest.get(appUrl('/amt-tiltak/api/tiltaksarrangor/gjennomforing/tilgjengelig'), (_req, res, ctx) => {
 		const gjennomforinger = [ mockGjennomforinger[0], ...mockTilgjengeligGjennomforinger ]
 		return res(ctx.delay(500), ctx.json(gjennomforinger))
 	}),
-	rest.get(appUrl('/amt-tiltak/api/gjennomforing/:gjennomforingId'), (req, res, ctx) => {
+	rest.get(appUrl('/amt-tiltak/api/tiltaksarrangor/gjennomforing/:gjennomforingId'), (req, res, ctx) => {
 		const gjennomforingId = req.params.gjennomforingId
 		const gjennomforing = mockGjennomforinger.find(g => g.id === gjennomforingId)
 
 		return res(ctx.delay(500), ctx.json(gjennomforing))
 	}),
-	rest.get(appUrl('/amt-tiltak/api/gjennomforing/:gjennomforingId/deltakere'), (req, res, ctx) => {
+	rest.get(appUrl('/amt-tiltak/api/tiltaksarrangor/gjennomforing/:gjennomforingId/deltakere'), (req, res, ctx) => {
 		const gjennomforingId = req.params.gjennomforingId
 
 		const data: TiltakDeltaker[] = mockTiltakDeltagere
@@ -36,10 +36,10 @@ export const mockHandlers: RequestHandler[] = [
 
 		return res(ctx.delay(500), ctx.json(data))
 	}),
-	rest.get(appUrl('/amt-tiltak/api/gjennomforing/:gjennomforingId/koordinatorer'), (req, res, ctx) => {
+	rest.get(appUrl('/amt-tiltak/api/tiltaksarrangor/gjennomforing/:gjennomforingId/koordinatorer'), (req, res, ctx) => {
 		return res(ctx.delay(500), ctx.json(mockKoordinatorer))
 	}),
-	rest.get(appUrl('/amt-tiltak/api/tiltak-deltaker/:deltakerId'), (req, res, ctx) => {
+	rest.get(appUrl('/amt-tiltak/api/tiltaksarrangor/deltaker/:deltakerId'), (req, res, ctx) => {
 		const deltakerId = req.params['deltakerId']
 		const deltaker = mockTiltakDeltagere.find((d) => d.id === deltakerId)! // eslint-disable-line @typescript-eslint/no-non-null-assertion
 		const gjennomforing = mockGjennomforinger.find(g => g.id === deltaker.gjennomforing.id)! // eslint-disable-line @typescript-eslint/no-non-null-assertion
@@ -47,13 +47,13 @@ export const mockHandlers: RequestHandler[] = [
 
 		return res(ctx.delay(500), ctx.json(deltakerMedGjennomforing))
 	}),
-	rest.get(appUrl('/amt-tiltak/api/gjennomforing'), (_req, res, ctx) => {
+	rest.get(appUrl('/amt-tiltak/api/tiltaksarrangor/gjennomforing'), (_req, res, ctx) => {
 		return res(ctx.delay(500), ctx.json(mockGjennomforinger))
 	}),
-	rest.post(appUrl('/amt-tiltak/api/gjennomforing/:gjennomforingId/tilgang'), (_req, res, ctx) => {
+	rest.post(appUrl('/amt-tiltak/api/tiltaksarrangor/gjennomforing/:gjennomforingId/tilgang'), (_req, res, ctx) => {
 		return res(ctx.delay(500), ctx.status(200))
 	}),
-	rest.delete(appUrl('/amt-tiltak/api/gjennomforing/:gjennomforingId/tilgang'), (_req, res, ctx) => {
+	rest.delete(appUrl('/amt-tiltak/api/tiltaksarrangor/gjennomforing/:gjennomforingId/tilgang'), (_req, res, ctx) => {
 		return res(ctx.delay(500), ctx.status(200))
 	}),
 	rest.get(appUrl('/amt-tiltak/api/tiltaksarrangor/endringsmelding'), (req, res, ctx) => {
