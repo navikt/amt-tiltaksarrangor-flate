05c565872cee3523c1bbabc72f11f27bacd14d23
Alexander Gård <alex.gaard@hotmail.com>
2022-11-29 12:37:39 +0100

    Refaktorering av msw request forward

--- src/mock/handlers/pull-request-handlers.ts
+++ src/mock/handlers/pull-request-handlers.ts
@@ -3,34 +3,20 @@ import { RequestHandler } from 'msw/lib/types/handlers/RequestHandler'
 import { MockedResponse } from 'msw/lib/types/response'
 
 import { appUrl } from '../../utils/url-utils'
+import { forwardRequest } from '../utils/request-utils'
 
 export const pullRequestHandlers: RequestHandler[] = [
-
+	rest.get(appUrl('/auth/info'), (req, res, ctx) => {
+		return handleReq(req, res, ctx)
+	}),
 	rest.all(appUrl('/amt-tiltak/*'), async(req, res, ctx) => {
 		return handleReq(req, res, ctx)
 	})
-
 ]
 
 const handleReq = async(req: RestRequest, res: ResponseComposition, ctx: RestContext): Promise<MockedResponse> => {
 	const url = req.url.pathname.replace(/\/pr-\d+/, '')
 	const proxiedUrl = `${url}${req.url.search}`
 
-	try {
-
-		const response = await ctx.fetch(proxiedUrl, {
-			credentials: 'include',
-			method: req.method,
-			body: req.body ? JSON.stringify(req.body) : null,
-			headers: req.headers
-		})
-
-		const bodyText = await response.text()
-
-		return res(ctx.status(response.status), ctx.body(bodyText))
-	} catch (e) {
-		// eslint-disable-next-line no-console
-		console.error('Request to proxy failed', e)
-		return res(ctx.status(500))
-	}
+	return forwardRequest(proxiedUrl, req, res, ctx)
 }
