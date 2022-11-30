fb14c1391fe9ec5087405641b49fe11fc1aeea5d
Izis Frøyhov <izis.froyhov@nav.no>
2022-11-30 12:17:56 +0100

    Flytter alertstripe (#202)

--- src/component/page/bruker-detaljer/deltaker-detaljer/DeltakelseInfo.tsx
+++ src/component/page/bruker-detaljer/deltaker-detaljer/DeltakelseInfo.tsx
@@ -1,8 +1,9 @@
-import { BodyShort } from '@navikt/ds-react'
+import { Alert, BodyShort } from '@navikt/ds-react'
 import React, { useState } from 'react'
 
-import { DeltakerStatus, TiltakDeltakerDetaljer } from '../../../../api/data/deltaker'
+import { DeltakerStatus, TiltakDeltakerDetaljer, TiltakDeltakerStatus } from '../../../../api/data/deltaker'
 import { formatDate } from '../../../../utils/date-utils'
+import { Nullable } from '../../../../utils/types/or-nothing'
 import { StatusMerkelapp } from '../../../felles/status-merkelapp/StatusMerkelapp'
 import styles from './DeltakelseInfo.module.scss'
 import { ElementPanel } from './ElementPanel'
@@ -13,12 +14,14 @@ interface DeltakelseInfoProps {
 	erSkjermetPerson: boolean
 	deltaker: TiltakDeltakerDetaljer
 	status: DeltakerStatus
+	fjernesDato: Nullable<Date>
 }
 
 export const DeltakelseInfo = ({
 	erSkjermetPerson,
 	deltaker,
-	status
+	status,
+	fjernesDato
 }: DeltakelseInfoProps): React.ReactElement => {
 	const [ key, setKey ] = useState<number>(0)
 	const reInstansierEndringsmeldinger = () => {
@@ -34,9 +37,16 @@ export const DeltakelseInfo = ({
 				<ElementPanel tittel="Dato:">
 					<BodyShort size="small" >{formatDate(deltaker.startDato)} - {formatDate(deltaker.sluttDato)}</BodyShort>
 				</ElementPanel>
+
 				<Endringsmeldinger deltakerId={deltaker.id} key={key}/>
+
+				{ erIkkeAktuellEllerHarSluttet(status.type) &&
+                    <Alert variant="warning" size="small">Deltakeren fjernes fra listen {formatDate(fjernesDato)}</Alert>}
 			</div>
 			<EndreDeltakelseKnapp disabled={erSkjermetPerson} deltaker={deltaker} onEndringUtfort={reInstansierEndringsmeldinger}/>
 		</div>
 	)
 }
+
+const erIkkeAktuellEllerHarSluttet = (status: TiltakDeltakerStatus): boolean =>
+	[ TiltakDeltakerStatus.IKKE_AKTUELL, TiltakDeltakerStatus.HAR_SLUTTET ].includes(status)
