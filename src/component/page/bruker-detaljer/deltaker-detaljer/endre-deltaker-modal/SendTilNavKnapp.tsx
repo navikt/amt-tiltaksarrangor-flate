f0c718d8fb906018a3475b95e0675db4dc602e3c
Alexander Gård <alex.gaard@hotmail.com>
2022-12-13 16:09:20 +0100

    Ikke tillat fraksjonstall for deltakelsesprosent

--- src/component/page/bruker-detaljer/deltaker-detaljer/endre-deltaker-modal/SendTilNavKnapp.tsx
+++ src/component/page/bruker-detaljer/deltaker-detaljer/endre-deltaker-modal/SendTilNavKnapp.tsx
@@ -1,5 +1,5 @@
-import { Button } from '@navikt/ds-react'
-import React from 'react'
+import { Alert, Button } from '@navikt/ds-react'
+import React, { useState } from 'react'
 
 import { isPending, usePromise } from '../../../../../utils/use-promise'
 
@@ -7,27 +7,35 @@ interface SendTilNavKnappProps {
 	onEndringSendt: () => void
 	sendEndring: () => Promise<void>
 	disabled: boolean
-
 }
 
 export const SendTilNavKnapp = (props: SendTilNavKnappProps) => {
 	const { disabled, sendEndring } = props
+	const [ showError, setShowError ] = useState<boolean>()
 	const opprettEndringPromise = usePromise<void>()
 
 	const onClick = () => {
 		opprettEndringPromise.setPromise(
-			sendEndring().then(props.onEndringSendt)
+			sendEndring()
+				.then(() => {
+					props.onEndringSendt()
+					setShowError(false)
+				})
+				.catch(() => setShowError(true))
 		)
 	}
 
 	return (
-		<Button
-			variant="primary"
-			size="small"
-			loading={isPending(opprettEndringPromise)}
-			onClick={onClick}
-			disabled={disabled}>
-			Send til NAV
-		</Button>
+		<>
+			<Button
+				variant="primary"
+				size="small"
+				loading={isPending(opprettEndringPromise)}
+				onClick={onClick}
+				disabled={disabled}>
+				Send til NAV
+			</Button>
+			{showError && <Alert variant="error">Noe gikk galt</Alert>}
+		</>
 	)
 }
