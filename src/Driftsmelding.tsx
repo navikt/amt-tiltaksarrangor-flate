afd11ab3e76f824c9d132e194a6eac91c202452d
Tormod Fossum <tormod.fossum@nav.no>
2023-01-10 15:25:49 +0100

    Legger in driftsmelding før relast av deltakere

--- src/Driftsmelding.tsx
+++ src/Driftsmelding.tsx
@@ -5,12 +5,20 @@ import styles from './Driftsmelding.module.scss'
 import { useFeatureToggle } from './hooks/useFeatureToggle'
 export const Driftsmelding = () => {
 	const { visDriftsmelding } = useFeatureToggle()
-	if(!visDriftsmelding) return <></>
+	if (!visDriftsmelding) return <></>
+
 
 	return (
 		<Alert variant="error" className={styles.alertstripe}>
-			På grunn av en forsinkelse i dataoverføring vil du kunne oppleve feil. <br/>
-			Vi jobber med feilsøking og retting. Beklager ulempen dette medfører.
+			På grunn av en teknisk oppdatering vil det mangle data om deltakere i omtrent en dag.<br />
+			Vi gir beskjed når oppdateringen er ferdig.
 		</Alert>
 	)
 }
+
+/*
+ * Original tekst:
+ *
+			På grunn av en forsinkelse i dataoverføring vil du kunne oppleve feil. <br/>
+			Vi jobber med feilsøking og retting. Beklager ulempen dette medfører.
+*/
