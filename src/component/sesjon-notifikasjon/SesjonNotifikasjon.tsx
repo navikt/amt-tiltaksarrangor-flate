692abf0ccac001ba3cb2f4fa8f52c629877c9bab
Izis Frøyhov <izis.froyhov@nav.no>
2023-02-24 11:19:37 +0100

    fikser feil som gjør at man havner på du-er-logget-ut siden når man blir logget inn (#263)

--- src/component/sesjon-notifikasjon/SesjonNotifikasjon.tsx
+++ src/component/sesjon-notifikasjon/SesjonNotifikasjon.tsx
@@ -51,8 +51,9 @@ export const SesjonNotifikasjon = (): React.ReactElement | null => {
 		tvungenUtloggingTimeoutRef.current = setInterval(() => {
 			if (tokenTimedOut()) {
 				setAlertType(AlertType.LOGGET_UT)
-				navigate(DU_ER_LOGGET_UT_PAGE_ROUTE)
 				window.location.href = loginUrl(window.location.href) //denne fungerer ikke om fanen ikke er aktiv, derfor navigerer man også internt
+				navigate(DU_ER_LOGGET_UT_PAGE_ROUTE)
+
 			}
 			else if (visDuBlirLoggetUtAlert()) {
 				setAlertType(AlertType.TVUNGEN_UTLOGGING_SNART)
