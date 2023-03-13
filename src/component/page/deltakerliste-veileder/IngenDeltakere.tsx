4c187a7046c3322122515fca79ca355a000efd11
Tia Firing <tia.firing@nav.no>
2023-03-13 14:54:34 +0100

    designforbedringer

--- src/component/page/deltakerliste-veileder/IngenDeltakere.tsx
+++ src/component/page/deltakerliste-veileder/IngenDeltakere.tsx
@@ -6,7 +6,7 @@ export const IngenDeltakere = (): JSX.Element => (
 	<div className={styles.ingenDeltakere}>
 		<GuidePanel poster>
         Du er logget inn som veileder i deltakeroversikten. Her kan du holde oversikt over deltakere som du følger opp.
-
+			<p />
         Du er ikke veileder til noen deltakere nå. Koordinator kan tildele deg som veileder til deltakere.
 		</GuidePanel>
 	</div>
