13725908c4e46945e279e10fb93b677b2865d55a
Tia Firing <tia.firing@nav.no>
2023-05-04 10:57:44 +0200

    retter feil i mockdata

--- src/mock/data/index.ts
+++ src/mock/data/index.ts
@@ -1,8 +1,11 @@
 import { Koordinator, Tiltakskode } from '../../api/data/tiltak'
 import { lagMockTiltakDeltagereForGjennomforing, MockTiltakDeltaker } from './brukere'
 import {
-	gjennomforingInfoListe, lagMockDeltakerlisteVeileder, lagMockMineDeltakerlister,
-	lagMockGjennomforinger, lagMockKoordinatorer,
+	gjennomforingInfoListe,
+	lagMockDeltakerlisteVeileder,
+	lagMockGjennomforinger,
+	lagMockKoordinatorer,
+	lagMockMineDeltakerlister,
 	MockGjennomforing
 } from './tiltak'
 import { KoordinatorsDeltakerliste, MineDeltakerlister, VeiledersDeltaker } from '../../api/data/deltaker'
@@ -29,20 +32,23 @@ export const mockKoordinatorsDeltakerliste = (gjennomforing: MockGjennomforing):
 }
 export const mockGjennomforinger: MockGjennomforing[] = lagMockGjennomforinger(gjennomforingInfoListe)
 
-export const lagMockTiltakDeltakere = (antallDeltakere: number): MockTiltakDeltaker[] => {
-	return mockGjennomforinger
+export const lagMockTiltakDeltakere = (antallGjennomforinger: number, antallDeltakere: number): MockTiltakDeltaker[] => {
+	return mockGjennomforinger.slice(0, antallGjennomforinger)
 		.map(gjennomforing => lagMockTiltakDeltagereForGjennomforing(gjennomforing, antallDeltakere))
 		.reduce((previousValue, currentValue) => previousValue.concat(currentValue), [])
 }
 
 const deltakereTilVeileder = () => {
-	const deltakere1 = lagMockTiltakDeltagereForGjennomforing(mockGjennomforinger[0], 10)
-	const deltakere2 = lagMockTiltakDeltagereForGjennomforing(mockGjennomforinger[1], 5)
+	const g1 = mockTiltakDeltakere[0].gjennomforing.id
+	const g2 = mockTiltakDeltakere.find(d => d.gjennomforing.id !== g1)?.gjennomforing.id
+
+	const deltakere1 = mockTiltakDeltakere.filter(d => d.gjennomforing.id === g1).slice(0, 10)
+	const deltakere2 = mockTiltakDeltakere.filter(d => d.gjennomforing.id === g2).slice(0, 5)
 
 	return lagMockDeltakerlisteVeileder(deltakere1.concat(deltakere2))
 }
 
-export const mockTiltakDeltakere: MockTiltakDeltaker[] = lagMockTiltakDeltakere(50)
+export const mockTiltakDeltakere: MockTiltakDeltaker[] = lagMockTiltakDeltakere(9, 100)
 
 export const mockDeltakerlisteVeileder: VeiledersDeltaker[] = deltakereTilVeileder()
 
