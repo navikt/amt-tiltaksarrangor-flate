f45e226f99f78db94e46f69269f5d900ec33ba2a
Izis Frøyhov <izis.froyhov@nav.no>
2023-06-30 13:18:50 +0200

    Retter feil som gjør at brukere får feilmelding dersom rollefetch ikke har fullført innen man forsøker å hente ut deltakerlister (#356)

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import { AxiosResponse } from 'axios'
 import React, { useEffect, useState } from 'react'
-import { fetchDeltakeroversikt, fetchMineRoller } from './api/tiltak-api'
+import { fetchMineRoller } from './api/tiltak-api'
 import { Header } from './component/felles/header/Header'
 import { AppRoutes } from './Routes'
 import { isNotStartedOrPending, isRejected, isResolved, usePromise } from './utils/use-promise'
 import { SesjonNotifikasjon } from './component/sesjon-notifikasjon/SesjonNotifikasjon'
 import { Rolle } from './api/data/ansatt'
 import { useInnloggetBrukerStore } from './store/innlogget-bruker-store'
-import { MineDeltakerlister } from './api/data/deltaker'
 import { useKoordinatorsDeltakerlisterStore } from './store/koordinators-deltakerlister-store'
+import { SpinnerPage } from './component/felles/spinner-page/SpinnerPage'
+import { ErrorPage } from './component/page/error/ErrorPage'
 
 
 export const App = (): React.ReactElement => {
 	const fetchMineRollerPromise = usePromise<AxiosResponse<Rolle[]>>(fetchMineRoller)
-	const fetchMineDeltakerlisterPromise = usePromise<AxiosResponse<MineDeltakerlister>>(
-		() => fetchDeltakeroversikt()
-	)
 	const { roller, setRoller } = useInnloggetBrukerStore()
-	const { setKoordinatorsDeltakerlister } = useKoordinatorsDeltakerlisterStore()
-
+	const { fetchDeltakerlister } = useKoordinatorsDeltakerlisterStore()
 	const [ isLoggedIn, setIsLoggedIn ] = useState<boolean>(false)
 
 	useEffect(() => {
@@ -26,24 +23,24 @@ export const App = (): React.ReactElement => {
 			setRoller(fetchMineRollerPromise.result.data)
 			setIsLoggedIn(true)
 		}
-		if (isResolved(fetchMineDeltakerlisterPromise) && fetchMineDeltakerlisterPromise.result.data) {
-			setKoordinatorsDeltakerlister(fetchMineDeltakerlisterPromise.result.data)
-		}
-
 		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [ fetchMineRollerPromise.result, fetchMineDeltakerlisterPromise.result ])
+	}, [ fetchMineRollerPromise.result ])
+
+	useEffect(() => {
+		if(roller && roller.find(x => x == Rolle.KOORDINATOR)) {
+			fetchDeltakerlister()
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [ roller ])
 
 	return (
 		<>
 			<Header isLoggedIn={isLoggedIn}/>
 			<main>
 				<SesjonNotifikasjon/>
-				<AppRoutes
-					// Vi må vente på at innloggetAnsatt er lagt inn i storen før vi rendrer routes
-					isLoading={isNotStartedOrPending(fetchMineRollerPromise) || !isLoggedIn}
-					isRejected={isRejected(fetchMineRollerPromise)}
-					roller={roller}
-				/>
+				{ isNotStartedOrPending(fetchMineRollerPromise) && <SpinnerPage />}
+				{ isRejected(fetchMineRollerPromise) && <ErrorPage />}
+				{ isLoggedIn && <AppRoutes roller={roller}/> }
 			</main>
 		</>
 	)
--- src/store/koordinators-deltakerlister-store.ts
+++ src/store/koordinators-deltakerlister-store.ts
@@ -1,11 +1,27 @@
 import constate from 'constate'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { MineDeltakerlister } from '../api/data/deltaker'
+import { fetchDeltakeroversikt } from '../api/tiltak-api'
+import { isResolved, usePromise } from '../utils/use-promise'
+import { AxiosResponse } from 'axios'
 
 export const [ KoordinatorsDeltakerlisterStoreProvider, useKoordinatorsDeltakerlisterStore ] = constate(() => {
 	const [ koordinatorsDeltakerlister, setKoordinatorsDeltakerlister ] = useState<MineDeltakerlister>()
+	const fetchMineDeltakerlisterPromise = usePromise<AxiosResponse<MineDeltakerlister>>()
+
+	const fetchDeltakerlister = () => fetchMineDeltakerlisterPromise.setPromise(fetchDeltakeroversikt)
+
+	useEffect(() => {
+		if(isResolved(fetchMineDeltakerlisterPromise)) {
+			setKoordinatorsDeltakerlister(fetchMineDeltakerlisterPromise.result.data)
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [ fetchMineDeltakerlisterPromise.result ])
+
 
 	return {
+		fetchDeltakerlister,
+		fetchMineDeltakerlisterPromise,
 		koordinatorsDeltakerlister,
 		setKoordinatorsDeltakerlister
 	}
