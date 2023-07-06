7778647dc523ee5772d09ea8871aad466f1cbbd3
MartheFH <136322563+MartheFH@users.noreply.github.com>
2023-07-06 09:03:52 +0200

    Håndtere å fjerne veileder for deltaker (#358)

--- src/component/page/bruker-detaljer/tildel-veileder-modal/TildelVeilederModal.tsx
+++ src/component/page/bruker-detaljer/tildel-veileder-modal/TildelVeilederModal.tsx
@@ -12,7 +12,6 @@ import { Deltaker } from '../../../../api/data/deltaker'
 import { SelectField, SelectOption } from '../../../felles/select-field/SelectField'
 import { BaseModal } from '../../../felles/base-modal/BaseModal'
 
-
 interface Props {
 	deltaker: Deltaker
 	veileder: Veileder | undefined
@@ -25,16 +24,15 @@ interface Props {
 const maksMedveiledere = 3
 
 export const TildelVeilederModal = (props: Props): React.ReactElement => {
-	const [ tilgjengeligeVeiledere, setTilgjengeligeVeiledere ] = useState<TilgjengeligVeileder[]>([])
-	const [ muligeVeiledervalg, setMuligeVeiledervalg ] = useState<SelectOption[]>()
-	const [ veileder, setVeileder ] = useState<TilgjengeligVeileder | undefined>()
-	const [ medveiledere, setMedveiledere ] = useState<TilgjengeligVeileder[]>([])
-	const [ maaVelgeVeilederError, setMaaVelgeVeilederError ] = useState(false)
-	const [ tildelingFeiletError, setTildelingFeiletError ] = useState(false)
+	const [tilgjengeligeVeiledere, setTilgjengeligeVeiledere] = useState<TilgjengeligVeileder[]>([])
+	const [muligeVeiledervalg, setMuligeVeiledervalg] = useState<SelectOption[]>()
+	const [veileder, setVeileder] = useState<TilgjengeligVeileder | undefined>()
+	const [medveiledere, setMedveiledere] = useState<TilgjengeligVeileder[]>([])
+	const [tildelingFeiletError, setTildelingFeiletError] = useState(false)
 
 	const tildelVeilederePromise = usePromise<void>()
-	const tilgjengeligeVeilederePromise = usePromise<AxiosResponse<TilgjengeligVeileder[]>>(
-		() => hentTilgjengeligeVeiledere(props.deltaker.deltakerliste.id)
+	const tilgjengeligeVeilederePromise = usePromise<AxiosResponse<TilgjengeligVeileder[]>>(() =>
+		hentTilgjengeligeVeiledere(props.deltaker.deltakerliste.id)
 	)
 
 	useEffect(() => {
@@ -42,7 +40,7 @@ export const TildelVeilederModal = (props: Props): React.ReactElement => {
 			setTilgjengeligeVeiledere(tilgjengeligeVeilederePromise.result.data)
 		}
 		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [ tilgjengeligeVeilederePromise.result ])
+	}, [tilgjengeligeVeilederePromise.result])
 
 	useEffect(() => {
 		if (props.veileder) {
@@ -51,45 +49,40 @@ export const TildelVeilederModal = (props: Props): React.ReactElement => {
 		if (props.medveiledere) {
 			setMedveiledere(props.medveiledere)
 		}
-	}, [ props.veileder, props.medveiledere ])
+	}, [props.veileder, props.medveiledere])
 
 	useEffect(() => {
-		const medveilederIder = medveiledere.map(v => v.ansattId)
+		const medveilederIder = medveiledere.map((v) => v.ansattId)
 		if (veileder !== undefined) medveilederIder.push(veileder.ansattId)
 
-		const muligeValg = tilgjengeligeVeiledere?.filter(v => !medveilederIder.includes(v.ansattId))
+		const muligeValg = tilgjengeligeVeiledere?.filter((v) => !medveilederIder.includes(v.ansattId))
 
 		setMuligeVeiledervalg(muligeValg.map(veilederToOption))
-
-	}, [ veileder, medveiledere, tilgjengeligeVeiledere ])
+	}, [veileder, medveiledere, tilgjengeligeVeiledere])
 
 	const handleVeilederChange = (valg: SingleValue<SelectOption>) => {
-		setVeileder(tilgjengeligeVeiledere?.find(v => v.ansattId === valg?.value))
-		setMaaVelgeVeilederError(false)
+		setVeileder(tilgjengeligeVeiledere?.find((v) => v.ansattId === valg?.value))
 	}
 
 	const handleMedveilederChange = (valg: MultiValue<SelectOption>) => {
-		const valgteMedveiledere = valg.map(v => tilgjengeligeVeiledere?.find(tv => tv.ansattId === v.value))
+		const valgteMedveiledere = valg.map((v) => tilgjengeligeVeiledere?.find((tv) => tv.ansattId === v.value))
 		setMedveiledere(valgteMedveiledere as TilgjengeligVeileder[])
 	}
 
 	const handleClose = () => {
 		setVeileder(props.veileder)
 		setMedveiledere(props.medveiledere)
-		setMaaVelgeVeilederError(false)
 		setTildelingFeiletError(false)
 		props.onClose()
 	}
 
 	const handleSubmit = () => {
-		if (!veileder) {
-			setMaaVelgeVeilederError(true)
-			return
-		}
 		if (medveiledere.length > maksMedveiledere) return
 
-		const veiledere: Veileder[] = [ { ...veileder, deltakerId: props.deltaker.id, erMedveileder: false } ]
-		medveiledere.forEach(v => veiledere.push({ ...v, deltakerId: props.deltaker.id, erMedveileder: true }))
+		const veiledere: Veileder[] = veileder
+			? [{ ...veileder, deltakerId: props.deltaker.id, erMedveileder: false }]
+			: []
+		medveiledere.forEach((v) => veiledere.push({ ...v, deltakerId: props.deltaker.id, erMedveileder: true }))
 
 		tildelVeilederePromise.setPromise(
 			tildelVeilederForDeltaker(props.deltaker.id, veiledere)
@@ -106,35 +99,47 @@ export const TildelVeilederModal = (props: Props): React.ReactElement => {
 		<>
 			<BaseModal tittel={'Endre veiledere'} open={props.open} onClose={handleClose} className={styles.modal}>
 				<Detail className={styles.detail}>
-					Tildel til {lagBrukerNavn(props.deltaker.fornavn, props.deltaker.mellomnavn, props.deltaker.etternavn)}
+					Tildel til{' '}
+					{lagBrukerNavn(props.deltaker.fornavn, props.deltaker.mellomnavn, props.deltaker.etternavn)}
 				</Detail>
 				<SelectField
 					label="Veileder"
-					isClearable={false}
+					isClearable
 					value={veileder ? veilederToOption(veileder) : undefined}
 					options={muligeVeiledervalg}
-					onChange={handleVeilederChange as (valg: SingleValue<SelectOption> | MultiValue<SelectOption>) => void}
+					onChange={
+						handleVeilederChange as (valg: SingleValue<SelectOption> | MultiValue<SelectOption>) => void
+					}
 					className={styles.select}
-					isError={maaVelgeVeilederError}
-					feilmelding="Du må velge en veileder"
 				/>
 				<SelectField
 					label="Medveiledere (valgfritt)"
 					isMulti
 					value={medveiledere.map(veilederToOption)}
 					options={muligeVeiledervalg}
-					onChange={handleMedveilederChange as (valg: SingleValue<SelectOption> | MultiValue<SelectOption>) => void}
+					onChange={
+						handleMedveilederChange as (valg: SingleValue<SelectOption> | MultiValue<SelectOption>) => void
+					}
 					className={styles.select}
 					isError={medveiledere.length > maksMedveiledere}
 					feilmelding="Deltaker kan ha maks 3 medveiledere"
 				/>
 				<Alert variant="info" className={styles.alert}>
-					Finner du ikke veilederen du leter etter? Veilederen må først få riktig rettighet i Altinn, og deretter logge inn i deltakeroversikten.
+					Finner du ikke veilederen du leter etter? Veilederen må først få riktig rettighet i Altinn, og
+					deretter logge inn i deltakeroversikten.
 				</Alert>
-				{tildelingFeiletError && <Alert variant="error" size="small">Kunne ikke tildele veiledere. Prøv igjen eller kontakt brukerstøtte </Alert>}
+				{tildelingFeiletError && (
+					<Alert variant="error" size="small">
+						Kunne ikke tildele veiledere. Prøv igjen eller kontakt brukerstøtte{' '}
+					</Alert>
+				)}
 				<div className={styles.buttonRow}>
-					<Button variant="tertiary" size="small" onClick={handleClose}>Avbryt</Button>
-					<Button size="small" loading={isPending(tildelVeilederePromise)} onClick={handleSubmit}>Lagre</Button>
+					<Button variant="tertiary" size="small" onClick={handleClose}>
+						Avbryt
+					</Button>
+					<Button size="small" loading={isPending(tildelVeilederePromise)} onClick={handleSubmit}>
+						Lagre
+					</Button>
 				</div>
 			</BaseModal>
 		</>
