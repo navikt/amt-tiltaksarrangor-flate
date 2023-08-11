f5dae45f4a767324b32b7710e971b38b5c5ae50a
Marthe Frogner Holm <marthe@Marthe-sin-MBP.localdomain>
2023-08-11 11:03:47 +0200

    legge til aria-live for filtreing

--- src/component/page/veileder/MineDeltakereTabell.tsx
+++ src/component/page/veileder/MineDeltakereTabell.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react'
 
 import { useTiltaksoversiktSokStore } from '../../../store/tiltaksoversikt-sok-store'
 import { finnNesteSortering } from '../../../utils/sortering-utils'
+import globalStyles from '../../../globals.module.scss'
 import styles from './MineDeltakereTabell.module.scss'
 import { sorterVeiledersDeltakere } from './sortering'
 import { VeiledersDeltaker } from '../../../api/data/deltaker'
@@ -11,7 +12,6 @@ import { TabellHeaderVeileder } from './TabellHeaderVeileder'
 import { TabellBodyVeileder } from './TabellBodyVeileder'
 import { filtrerDeltakerliste } from '../../../utils/filtrering-utils'
 import { useVeilederFilterMenyStore } from './store/veileder-filter-meny-store-provider'
-
 
 interface MineDeltakereTabellProps {
 	mineDeltakere: VeiledersDeltaker[]
@@ -29,7 +29,6 @@ export const MineDeltakereTabell = (props: MineDeltakereTabellProps): React.Reac
 		const filtrerteDeltakerlister = filtrerDeltakerliste(filtrerteBrukere, deltakerlisteFilter)
 		const sortert = sorterVeiledersDeltakere(filtrerteDeltakerlister, deltakerSortering)
 		setDeltakereBearbeidet(sortert)
-
 	}, [ filtrerDeltakere, statusFilter, mineDeltakere, deltakerSortering, deltakerlisteFilter, veiledertypeFilter ])
 
 	const handleOnSortChange = (sortKey: string | undefined) => {
@@ -41,12 +40,22 @@ export const MineDeltakereTabell = (props: MineDeltakereTabellProps): React.Reac
 			{mineDeltakere.length === 0
 				? <IngenDeltakere/>
 				: (
-					<Table className="tabell" zebraStripes={true} sort={deltakerSortering} onSortChange={handleOnSortChange} aria-label="Deltakere på tiltaksgjennomføring">
-						<TabellHeaderVeileder/>
-						<TabellBodyVeileder brukere={deltakereBearbeidet}/>
-					</Table>
-				)
-			}
+					<>
+						<Table
+							className="tabell"
+							zebraStripes={true}
+							sort={deltakerSortering}
+							onSortChange={handleOnSortChange}
+							aria-label="Deltakere på tiltaksgjennomføring"
+						>
+							<TabellHeaderVeileder />
+							<TabellBodyVeileder brukere={deltakereBearbeidet} />
+						</Table>
+						<div aria-live="polite" aria-atomic="true" className={globalStyles.screenReaderOnly}>
+							Viser {deltakereBearbeidet.length} av {mineDeltakere.length} deltakere.
+						</div>
+					</>
+				)}
 		</div>
 	)
 }
