e7c2ee1086facf0432e65e601cd48ee6c8813b44
Marthe Frogner Holm <marthe@Marthe-sin-MBP.localdomain>
2023-08-29 14:51:12 +0200

    Små fiks tekst størrelse og ikon

--- src/component/page/bruker-detaljer/DeltakerAdresse.tsx
+++ src/component/page/bruker-detaljer/DeltakerAdresse.tsx
@@ -1,10 +1,11 @@
 import { HouseIcon } from '@navikt/aksel-icons'
-import { Label } from '@navikt/ds-react'
+import { BodyShort, Label } from '@navikt/ds-react'
 import React, { useId } from 'react'
 import styles from './DeltakerDetaljerHeader.module.scss'
 import { Adresse } from '../../../api/data/deltaker'
 import { mapAdresseTypeTilTekst } from '../../../utils/text-mappers'
 import { lagAdresseTekst } from '../../../utils/deltaker-utils'
+import { EMDASH } from '../../../utils/constants'
 
 interface DeltakerAdresseProps {
 	adresse?: Adresse | null
@@ -14,15 +15,15 @@ export const DeltakerAdresse = ( { adresse }: DeltakerAdresseProps ): React.Reac
 	const adresseId = useId()
 
 	return (
-		<div className={ styles.adresse }>
+		<div className={styles.adresse}>
 			<HouseIcon aria-hidden />
 			<div>
-				<Label htmlFor={ adresseId } as="span" size="small">
-					{ adresse ? mapAdresseTypeTilTekst( adresse?.adressetype ) : 'Adresse' }
-				</Label>{ ' ' }
-				<div id={ adresseId }>
-					{ adresse ? lagAdresseTekst( adresse ) : '-' }
-				</div>
+				<Label htmlFor={adresseId} as="span" size="small">
+					{adresse ? mapAdresseTypeTilTekst(adresse?.adressetype) : 'Adresse'}
+				</Label>{' '}
+				<BodyShort id={adresseId} size="small">
+					{adresse ? lagAdresseTekst(adresse) : EMDASH}
+				</BodyShort>
 			</div>
 		</div>
 	)
