ba6cc226e50a11d6b19850247e6462811ee48da2
Marthe Frogner Holm <marthe@Marthe-sin-MBP.localdomain>
2023-09-08 09:42:38 +0200

    fjern console

--- src/component/page/bruker-detaljer/tildel-veileder-modal/TildelVeilederModal.tsx
+++ src/component/page/bruker-detaljer/tildel-veileder-modal/TildelVeilederModal.tsx
@@ -70,7 +70,6 @@ export const TildelVeilederModal = (props: Props): React.ReactElement => {
 	}
 
 	const handleClose = () => {
-		console.log('Close modal handler')
 		setVeileder(props.veileder)
 		setMedveiledere(props.medveiledere)
 		setTildelingFeiletError(false)
