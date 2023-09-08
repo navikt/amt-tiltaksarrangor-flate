757136493ed4de71efe3b1dc3a3bf32dfd3cc9cf
MartheFH <136322563+MartheFH@users.noreply.github.com>
2023-09-08 10:13:06 +0200

    Smaa fix (#388)

--- src/component/felles/base-modal/BaseModal.tsx
+++ src/component/felles/base-modal/BaseModal.tsx
@@ -1,4 +1,4 @@
-import { Modal } from '@navikt/ds-react'
+import { Heading, Modal } from '@navikt/ds-react'
 import classNames from 'classnames'
 import React, { ReactNode } from 'react'
 
@@ -18,7 +18,12 @@ export const BaseModal = (props: BaseModalProps) => {
 	const isOpen = open ?? true
 
 	return (
-		<Modal open={isOpen} onClose={onClose} className={classNames(styles.container, props.className)} header={{ heading: tittel }}>
+		<Modal open={ isOpen } onClose={ onClose } className={ classNames( styles.container, props.className ) } aria-label={ tittel }>
+			<Modal.Header>
+				<Heading level="1" size="medium" className={styles.modalHeader}>
+					{ tittel }
+				</Heading>
+			</Modal.Header>
 			<Modal.Body className={contentClassName}>
 				<div className={styles.content}>{children}</div>
 			</Modal.Body>
