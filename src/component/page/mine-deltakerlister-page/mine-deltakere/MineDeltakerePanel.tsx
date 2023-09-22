e49c7187edb000fbf4e2053fe152731837de688e
Marthe Frogner Holm <marthe@Marthe-sin-MBP.localdomain>
2023-09-22 13:34:12 +0200

    Fjerne alt tekst

--- src/component/page/mine-deltakerlister-page/mine-deltakere/MineDeltakerePanel.tsx
+++ src/component/page/mine-deltakerlister-page/mine-deltakere/MineDeltakerePanel.tsx
@@ -17,7 +17,7 @@ export const MineDeltakerePanel = (props: MineDeltakerePanelProps): React.ReactE
 			<Link to={ MINE_DELTAKERE_PAGE_ROUTE } className={ styles.link } >
 				<LinkPanel border >
 					<div className={styles.linkpanel}>
-						<img src={clipboard} alt="Clipboard" className={styles.clipboardimage}/>
+						<img src={clipboard} alt="" className={styles.clipboardimage}/>
 						<div>
 							<LinkPanel.Title>Mine deltakere</LinkPanel.Title>
 							<LinkPanel.Description>Du er veileder for {props.veileder.veilederFor} deltakere og medveileder for {props.veileder.medveilederFor} deltakere.</LinkPanel.Description>
