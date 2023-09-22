a9ae92d4bdca8ff44c7e5e6be57e264470529315
Marthe Frogner Holm <marthe@Marthe-sin-MBP.localdomain>
2023-09-22 13:26:45 +0200

    bytt til router lenke

--- src/component/page/mine-deltakerlister-page/mine-deltakere/MineDeltakerePanel.tsx
+++ src/component/page/mine-deltakerlister-page/mine-deltakere/MineDeltakerePanel.tsx
@@ -1,6 +1,7 @@
 import { LinkPanel } from '@navikt/ds-react'
 import React from 'react'
 
+import { Link } from 'react-router-dom'
 import styles from './MineDeltakerePanel.module.scss'
 import { VeilederFor } from '../../../../api/data/deltaker'
 import { MINE_DELTAKERE_PAGE_ROUTE } from '../../../../navigation'
@@ -13,15 +14,17 @@ interface MineDeltakerePanelProps {
 export const MineDeltakerePanel = (props: MineDeltakerePanelProps): React.ReactElement<MineDeltakerePanelProps> => {
 	return (
 		<div className={styles.content} >
-			<LinkPanel href={MINE_DELTAKERE_PAGE_ROUTE} border>
-				<div className={styles.linkpanel}>
-					<img src={clipboard} alt="Clipboard" className={styles.clipboardimage}/>
-					<div>
-						<LinkPanel.Title>Mine deltakere</LinkPanel.Title>
-						<LinkPanel.Description>Du er veileder for {props.veileder.veilederFor} deltakere og medveileder for {props.veileder.medveilederFor} deltakere.</LinkPanel.Description>
+			<Link to={ MINE_DELTAKERE_PAGE_ROUTE } className={ styles.link } >
+				<LinkPanel border >
+					<div className={styles.linkpanel}>
+						<img src={clipboard} alt="Clipboard" className={styles.clipboardimage}/>
+						<div>
+							<LinkPanel.Title>Mine deltakere</LinkPanel.Title>
+							<LinkPanel.Description>Du er veileder for {props.veileder.veilederFor} deltakere og medveileder for {props.veileder.medveilederFor} deltakere.</LinkPanel.Description>
+						</div>
 					</div>
-				</div>
-			</LinkPanel>
+				</LinkPanel>
+			</Link>
 		</div>
 	)
 }
