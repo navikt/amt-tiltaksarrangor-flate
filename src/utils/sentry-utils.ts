2bc102b2c49b5cbf1a1cd54470d17f5fb433815a
Marthe Frogner Holm <marthe@Marthe-sin-MacBook-Pro.local>
2024-06-07 09:18:57 +0200

    fiks browserTracingIntegration

--- src/utils/sentry-utils.ts
+++ src/utils/sentry-utils.ts
@@ -1,12 +1,11 @@
 import * as Sentry from '@sentry/react'
-import { Integrations } from '@sentry/tracing'
 
 import env from './environment'
 
 export const initSentry = (): void => {
 	Sentry.init({
 		dsn: 'https://25638817aa76462089796015a4d3b6eb@sentry.gc.nav.no/102',
-		integrations: [ new Integrations.BrowserTracing() ],
+		integrations: [ Sentry.browserTracingIntegration() ],
 		environment: env.name,
 		tracesSampleRate: 0.25
 	})
