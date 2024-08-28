214ffc7670c56c0e052da8ff60575681e5c9ceee
Marthe Frogner Holm <marthe@Marthe-sin-MacBook-Pro.local>
2024-08-28 13:51:45 +0200

    Fiks link

--- src/component/felles/alert-info-message/AlertInfoMessage.tsx
+++ src/component/felles/alert-info-message/AlertInfoMessage.tsx
@@ -1,10 +1,10 @@
-import { Alert, Heading } from '@navikt/ds-react'
+import { Alert, BodyShort, Heading, Link } from '@navikt/ds-react'
 import useLocalStorage from '../../../hooks/useLocalStorage'
 import styles from './AlertInfoMessage.module.scss'
 
 export const AlertInfoMessage = () => {
-  const alertMessage = 'Dette påvirker deltakeroversikten. Du vil kunne sende forslag om endringer til NAV-veileder. \n Arbeidsforberedende trening er den første tiltakstypen i ny løsning. Les mer på nav.no her.'
-  const [ lastMessage ] = useLocalStorage(
+  const alertMessage = 'Dette påvirker deltakeroversikten. Du vil kunne sende forslag om endringer til NAV-veileder. \n Arbeidsforberedende trening er den første tiltakstypen i ny løsning.'
+  const [ lastMessage, setLastMessage ] = useLocalStorage(
     'alert-message-last-message',
     alertMessage
   )
@@ -12,12 +12,20 @@ export const AlertInfoMessage = () => {
     'alert-message-hide',
     false
   )
+
   const shouldShowAlertMessage = alertMessage === lastMessage
     ? !hideAlertMessage : true
 
+  if (alertMessage !== lastMessage) {
+    setLastMessage(alertMessage)
+    setHideAlertMessage(false)
+  }
+
   return shouldShowAlertMessage ? <Alert variant="info" size="small" closeButton className={styles.alert} onClose={() => setHideAlertMessage(true)}>
     <Heading size="xsmall" level="2">Ny påmeldingsløsning for NAV-veileder kommer 1.oktober</Heading>
-    {alertMessage}
+    <BodyShort size="small">
+      {alertMessage} <Link href="https://www.nav.no/nytt-i-deltakeroversikten">Les mer om endringene i deltakeroversikten på nav.no her.</Link>
+    </BodyShort>
   </Alert>
     : <></>
 
