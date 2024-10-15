d060a14e3874da98bc3c8ec4b856e2ec35734742
MartheFH <136322563+MartheFH@users.noreply.github.com>
2024-10-15 07:58:15 +0200

    Endre info til ta i alertene (#848)

--- src/component/felles/alert-info-message/AlertInfoMessage.tsx
+++ src/component/felles/alert-info-message/AlertInfoMessage.tsx
@@ -3,7 +3,7 @@ import useLocalStorage from '../../../hooks/useLocalStorage'
 import styles from './AlertInfoMessage.module.scss'
 
 export const AlertInfoMessage = () => {
-  const alertMessage = 'Dette påvirker i første omgang kun AFT-tiltak i deltakeroversikten. I overgangen til ny løsning vil knappen “Endre deltakelse” ikke være tilgjengelig fra 11. - 14. okt. Fra 15. okt. vil du kunne sende forslag om endring direkte til NAV-veilederen.'
+  const alertMessage = 'Dette påvirker i første omgang kun AFT-tiltak i deltakeroversikten. Når du endrer deltakelse så sendes forslaget direkte til NAV-veilederen.'
   const [ lastMessage, setLastMessage ] = useLocalStorage(
     'alert-message-last-message',
     alertMessage
@@ -21,15 +21,15 @@ export const AlertInfoMessage = () => {
     setHideAlertMessage(false)
   }
 
-  return shouldShowAlertMessage ? <Alert variant="info" size="small" closeButton className={styles.alert} onClose={() => setHideAlertMessage(true)}>
-    <Heading size="xsmall" level="2">Ny løsning for NAV-veileder kommer 15. oktober for Arbeidsforberedende trening (AFT)</Heading>
-    <BodyShort size="small" className={styles.text}>
-      {alertMessage}
-    </BodyShort>
-    <BodyShort size="small" className={styles.text}>
-      <Link href="https://www.nav.no/nytt-i-deltakeroversikten">Les mer om endringene i deltakeroversikten på nav.no her.</Link>
-    </BodyShort>
-  </Alert>
+  return shouldShowAlertMessage ?
+    <Alert variant="success" size="small" closeButton className={styles.alert} onClose={() => setHideAlertMessage(true)}>
+      <Heading size="xsmall" level="2">NAV-veileder har nå ny løsning for Arbeidsforberedende trening (AFT)</Heading>
+      <BodyShort size="small" className={styles.text}>
+        {alertMessage}
+      </BodyShort>
+      <BodyShort size="small" className={styles.text}>
+        <Link href="https://www.nav.no/nytt-i-deltakeroversikten">Les mer om endringene i deltakeroversikten på nav.no her.</Link>
+      </BodyShort>
+    </Alert>
     : <></>
-
 }
