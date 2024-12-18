30f8cc3115677f98398150cb770ce6088a185f99
Marthe Frogner Holm <marthe@Mac.localdomain>
2024-12-18 09:15:12 +0100

    Fiks bug med å velge bort og fiks modal

--- src/component/page/bruker-detaljer/tildel-veileder-modal/TildelVeilederModal.tsx
+++ src/component/page/bruker-detaljer/tildel-veileder-modal/TildelVeilederModal.tsx
@@ -1,21 +1,20 @@
-import { Alert, Button, Detail, ReadMore, UNSAFE_Combobox } from '@navikt/ds-react'
+import { Alert, Button, Detail, Modal, ReadMore, UNSAFE_Combobox } from '@navikt/ds-react'
 import React, { useEffect, useState } from 'react'
 import { TilgjengeligVeileder, Veileder } from '../../../../api/data/veileder'
 import { lagBrukerNavn } from '../../../../utils/bruker-utils'
 
-import styles from './TildelVeilederModal.module.scss'
+import { AxiosResponse } from 'axios'
+import { Deltaker } from '../../../../api/data/deltaker'
+import {
+  hentTilgjengeligeVeiledere,
+  tildelVeilederForDeltaker
+} from '../../../../api/tiltak-api'
 import {
   isPending,
   isResolved,
   usePromise
 } from '../../../../utils/use-promise'
-import { AxiosResponse } from 'axios'
-import {
-  hentTilgjengeligeVeiledere,
-  tildelVeilederForDeltaker
-} from '../../../../api/tiltak-api'
-import { Deltaker } from '../../../../api/data/deltaker'
-import { BaseModal } from '../../../felles/base-modal/BaseModal'
+import styles from './TildelVeilederModal.module.scss'
 
 export type SelectOption = {
   value: string
@@ -34,14 +33,14 @@ interface Props {
 const maksMedveiledere = 3
 
 export const TildelVeilederModal = (props: Props): React.ReactElement => {
-  const [tilgjengeligeVeiledere, setTilgjengeligeVeiledere] = useState<
+  const [ tilgjengeligeVeiledere, setTilgjengeligeVeiledere ] = useState<
     TilgjengeligVeileder[]
   >([])
 
-  const [muligeVeiledervalg, setMuligeVeiledervalg] = useState<SelectOption[]>()
-  const [veileder, setVeileder] = useState<TilgjengeligVeileder | undefined>()
-  const [medveiledere, setMedveiledere] = useState<TilgjengeligVeileder[]>([])
-  const [tildelingFeiletError, setTildelingFeiletError] = useState(false)
+  const [ muligeVeiledervalg, setMuligeVeiledervalg ] = useState<SelectOption[]>()
+  const [ veileder, setVeileder ] = useState<TilgjengeligVeileder | undefined>()
+  const [ medveiledere, setMedveiledere ] = useState<TilgjengeligVeileder[]>([])
+  const [ tildelingFeiletError, setTildelingFeiletError ] = useState(false)
 
   const tildelVeilederePromise = usePromise<void>()
   const tilgjengeligeVeilederePromise = usePromise<
@@ -52,7 +51,7 @@ export const TildelVeilederModal = (props: Props): React.ReactElement => {
     if (isResolved(tilgjengeligeVeilederePromise)) {
       setTilgjengeligeVeiledere(tilgjengeligeVeilederePromise.result.data)
     }
-  }, [tilgjengeligeVeilederePromise.result])
+  }, [ tilgjengeligeVeilederePromise.result ])
 
   useEffect(() => {
     if (props.veileder) {
@@ -61,7 +60,7 @@ export const TildelVeilederModal = (props: Props): React.ReactElement => {
     if (props.medveiledere) {
       setMedveiledere(props.medveiledere)
     }
-  }, [props.veileder, props.medveiledere])
+  }, [ props.veileder, props.medveiledere ])
 
   useEffect(() => {
     const medveilederIder = medveiledere.map((v) => v.ansattId)
@@ -72,10 +71,12 @@ export const TildelVeilederModal = (props: Props): React.ReactElement => {
     )
 
     setMuligeVeiledervalg(muligeValg.map(veilederToOption))
-  }, [veileder, medveiledere, tilgjengeligeVeiledere])
+  }, [ veileder, medveiledere, tilgjengeligeVeiledere ])
 
-  const handleVeilederChange = (valgtAnsattId: string) => {
-    setVeileder(tilgjengeligeVeiledere?.find((v) => v.ansattId === valgtAnsattId))
+  const handleVeilederChange = (valgtAnsattId: string, isSelected: boolean) => {
+    if (isSelected) {
+      setVeileder(tilgjengeligeVeiledere?.find((v) => v.ansattId === valgtAnsattId))
+    } else { setVeileder(undefined) }
   }
 
   const handleMedveilederChange = (valgtAnsattId: string, isSelected: boolean) => {
@@ -98,7 +99,7 @@ export const TildelVeilederModal = (props: Props): React.ReactElement => {
     if (medveiledere.length > maksMedveiledere) return
 
     const veiledere: Veileder[] = veileder
-      ? [{ ...veileder, deltakerId: props.deltaker.id, erMedveileder: false }]
+      ? [ { ...veileder, deltakerId: props.deltaker.id, erMedveileder: false } ]
       : []
     medveiledere.forEach((v) =>
       veiledere.push({
@@ -121,62 +122,66 @@ export const TildelVeilederModal = (props: Props): React.ReactElement => {
 
   return (
     <>
-      <BaseModal
-        tittel={'Endre veiledere'}
+      <Modal
+        header={{ heading: 'Endre veiledere' }}
         open={props.open}
         onClose={handleClose}
         className={styles.modal}
       >
-        <Detail className={styles.detail}>
-          Tildel til{' '}
-          {lagBrukerNavn(
-            props.deltaker.fornavn,
-            props.deltaker.mellomnavn,
-            props.deltaker.etternavn
-          )}
-        </Detail>
-        <UNSAFE_Combobox
-          label="Veileder"
-          size="small"
-          className={styles.select}
-          options={muligeVeiledervalg ?? []}
-          onToggleSelected={(option) => handleVeilederChange(option)}
-        />
-        <UNSAFE_Combobox
-          label="Medveiledere"
-          size="small"
-          className={styles.select}
-          options={muligeVeiledervalg ?? []}
-          selectedOptions={medveiledere.map(m => veilederToOption(m))}
-          onToggleSelected={(option, isSelected) => handleMedveilederChange(option, isSelected)}
-          isMultiSelect
-          maxSelected={3}
-        />
-        <ReadMore header="Finner du ikke riktig veileder?">
-          <p>
-            Veilederen må først få riktig rettighet i Altinn, og deretter logge
-            inn i Deltakeroversikten. Først da kan koordinatoren finne
-            veilederen.
-          </p>
-          <p>
-            Enkeltrettigheten i Altinn heter: <br />
-            <span className={styles.bold}>
-              “Tiltaksarrangør veileder - Nav Deltakeroversikt”.
-            </span>
-          </p>
-          <p>
-            Enkeltrettigheten må registreres på org.nr. til{' '}
-            <span className={styles.bold}>bedriftens underenhet</span> - det
-            samme org.nr. som avtalen om tiltaket er koblet til.
-          </p>
-        </ReadMore>
+        <Modal.Body className={styles.modalBody}>
+          <Detail className={styles.detail}>
+            Tildel til{' '}
+            {lagBrukerNavn(
+              props.deltaker.fornavn,
+              props.deltaker.mellomnavn,
+              props.deltaker.etternavn
+            )}
+          </Detail>
+          <UNSAFE_Combobox
+            label="Veileder"
+            size="small"
+            className={styles.select}
+            options={muligeVeiledervalg ?? []}
+            onToggleSelected={(option, isSelected) => {
+              handleVeilederChange(option, isSelected)
+            }}
+          />
+          <UNSAFE_Combobox
+            label="Medveiledere"
+            size="small"
+            className={styles.select}
+            options={muligeVeiledervalg ?? []}
+            selectedOptions={medveiledere.map(m => veilederToOption(m))}
+            onToggleSelected={(option, isSelected) => handleMedveilederChange(option, isSelected)}
+            isMultiSelect
+            maxSelected={3}
+          />
+          <ReadMore header="Finner du ikke riktig veileder?">
+            <p>
+              Veilederen må først få riktig rettighet i Altinn, og deretter logge
+              inn i Deltakeroversikten. Først da kan koordinatoren finne
+              veilederen.
+            </p>
+            <p>
+              Enkeltrettigheten i Altinn heter: <br />
+              <span className={styles.bold}>
+                “Tiltaksarrangør veileder - Nav Deltakeroversikt”.
+              </span>
+            </p>
+            <p>
+              Enkeltrettigheten må registreres på org.nr. til{' '}
+              <span className={styles.bold}>bedriftens underenhet</span> - det
+              samme org.nr. som avtalen om tiltaket er koblet til.
+            </p>
+          </ReadMore>
 
-        {tildelingFeiletError && (
-          <Alert variant="error" size="small">
-            Kunne ikke tildele veiledere. Prøv igjen eller kontakt brukerstøtte{' '}
-          </Alert>
-        )}
-        <div className={styles.buttonRow}>
+          {tildelingFeiletError && (
+            <Alert variant="error" size="small">
+              Kunne ikke tildele veiledere. Prøv igjen eller kontakt brukerstøtte{' '}
+            </Alert>
+          )}
+        </Modal.Body>
+        <Modal.Footer className={styles.buttonRow}>
           <Button variant="tertiary" size="small" onClick={handleClose}>
             Avbryt
           </Button>
@@ -187,8 +192,8 @@ export const TildelVeilederModal = (props: Props): React.ReactElement => {
           >
             Lagre
           </Button>
-        </div>
-      </BaseModal>
+        </Modal.Footer>
+      </Modal>
     </>
   )
 }
