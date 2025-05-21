03c5db57238e73dbe4a7fc4add3bce0f0b6d8df8
Izis Frøyhov <izis.froyhov@nav.no>
2025-05-21 10:27:32 +0200

    ny infomelding

--- src/component/felles/alert-info-message/sokt-inn-vurderes/AlertInfoMessageSoktInnVurderes.tsx
+++ src/component/felles/alert-info-message/sokt-inn-vurderes/AlertInfoMessageSoktInnVurderes.tsx
@@ -3,7 +3,7 @@ import useLocalStorage from '../../../../hooks/useLocalStorage'
 import styles from './AlertInfoMessageSoktInnVurderes.module.scss'
 
 export const AlertInfoMessageSoktInnVurderes = () => {
-  const alertMessage = 'Forslag til endringer på Gruppe-AMO, Fag- og yrkesopplæring og Jobbklubb sendes direkte til Nav-veileder. '
+  const alertMessage = 'I overgangen til ny løsning vil knappen “Endre deltakelse” ikke være tilgjengelig fra 28. mai - 2. juni. Fra 3. juni. vil du kunne sende forslag om endring direkte til NAV-veilederen.'
   const [ hideAlertMessage, setHideAlertMessage ] = useLocalStorage(
     'alert-message-sokt-inn-hide',
     false
@@ -13,9 +13,9 @@ export const AlertInfoMessageSoktInnVurderes = () => {
 
   return shouldShowAlertMessage ?
     <Alert variant="info" size="small" closeButton className={styles.alert} onClose={() => setHideAlertMessage(true)}>
-      <Heading size="xsmall" level="2">“Endre deltakelse” blir til “forslag om endring” 3. juni</Heading>
+      <Heading size="xsmall" level="2">Ny påmeldingsløsning for NAV-veileder kommer 3. juni for tiltakene gruppe-AMO, gruppe fag- og yrkesopplæring og jobbklubb</Heading>
       <BodyShort as="span" size="small" className={styles.text}>
-        {alertMessage} {' '}<Link href="https://www.nav.no/samarbeidspartner/nytt-i-deltakeroversikten">Les mer på nav.no.</Link>
+        {alertMessage} {' '}<Link href="https://www.nav.no/samarbeidspartner/nytt-i-deltakeroversikten">Les mer om endringene i deltakeroversikten på nav.no her.</Link>
       </BodyShort>
     </Alert>
     : <></>
