eb04ec0d7b99f219609ba61d9a45ac190408428b
MartheFH <136322563+MartheFH@users.noreply.github.com>
2025-06-02 12:53:43 +0200

    Fjern flere spesialtegn (#1131)

--- src/utils/endre-deltaker-utils.ts
+++ src/utils/endre-deltaker-utils.ts
@@ -4,7 +4,20 @@ export const BEGRUNNELSE_MAKS_TEGN = 350
 export const fjernUgyldigeTegn = (str: string) => {
     const ugyldigeTegnRegex =
         /[^a-zA-ZæøåÆØÅ0-9 \s.,@<>'`!"#$%&\/()\\=?*<;:>*§_-]/g
-    const stringUtenNonBreakingSace = str.replace(/\u00A0/g, ' ')
-    const stringUtenZeroWidthNonBreakingSace = stringUtenNonBreakingSace.replace(/\uFEFF/g, '')
-    return stringUtenZeroWidthNonBreakingSace.replace(ugyldigeTegnRegex, '')
+
+    const cleanText = str.replace(
+        new RegExp(
+            // NBSP, Narrow NBSP, ZWNBSP, Word Joiner, Zero-width space, ZWNJ, ZWJ
+            '[\\u00A0\\u202F\\uFEFF\\u2060\\u200B\\u200C\\u200D]|' +
+            ugyldigeTegnRegex.source,
+            'g'
+        ),
+        (match) => {
+            // NBSP (NonBreakingSace)og, Narrow NBSP
+            if (match === '\u00A0' || match === '\u202F') return ' '
+            return '' // Fjern alle andre tegn
+        }
+    )
+
+    return cleanText
 }
