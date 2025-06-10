63f06ecc3113685cc13b70d3e6da44f0eeb8d943
MartheFH <marthe.frogner.holm@nav.no>
2025-06-10 14:32:33 +0200

    test cypress fix

--- cypress.config.ts
+++ cypress.config.ts
@@ -10,13 +10,7 @@ export default defineConfig({
 	defaultCommandTimeout: 20000,
 	screenshotOnRunFailure: true,
 	video: true,
-	videoUploadOnPasses: false,
 	e2e: {
-		// We've imported your old cypress plugins here.
-		// You may want to clean this up later by importing these.
-		setupNodeEvents(on, config) {
-			return require('./cypress/plugins/index.js')(on, config)
-		},
 		baseUrl: 'http://localhost:3001/deltakeroversikt',
 		specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
 	},
