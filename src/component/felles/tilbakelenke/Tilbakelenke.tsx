75d2459d8bb14cb883e5290c6f1df3d8ad32be82
MartheFH <136322563+MartheFH@users.noreply.github.com>
2026-03-26 11:08:16 +0100

    Bump deps (#1307)
    * Bump deps
    
    * Fiks tilbakelenke styling

--- src/component/felles/tilbakelenke/Tilbakelenke.tsx
+++ src/component/felles/tilbakelenke/Tilbakelenke.tsx
@@ -1,8 +1,8 @@
 import { ChevronLeftIcon } from '@navikt/aksel-icons'
-import { Label } from '@navikt/ds-react'
+import { Label, Link } from '@navikt/ds-react'
 import cls from 'classnames'
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link as RouterLink } from 'react-router-dom'
 
 import styles from './Tilbakelenke.module.scss'
 
@@ -15,11 +15,12 @@ export const Tilbakelenke = (
   props: TilbakelenkeProps
 ): React.ReactElement<TilbakelenkeProps> => {
   return (
-    <Link to={props.to} className={cls(styles.tilbakelenke, props.className)}>
+    <Link as={RouterLink} to={props.to} className={cls(styles.tilbakelenke, props.className)}>
       <ChevronLeftIcon className={styles.pil} />
       <Label as="span" size="small">
         Tilbake
       </Label>
     </Link>
+
   )
 }
